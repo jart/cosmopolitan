95bc650be8210346ff968d2bce581bf7a4ab5106
Justine Tunney <jtunney@gmail.com>
2020-11-07 00:05:25 -0800

    Fix missing header include

--- tool/build/lib/pml4t.h
+++ tool/build/lib/pml4t.h
@@ -1,5 +1,6 @@
 #ifndef COSMOPOLITAN_TOOL_BUILD_LIB_PML4T_H_
 #define COSMOPOLITAN_TOOL_BUILD_LIB_PML4T_H_
+#include "tool/build/lib/machine.h"
 #if !(__ASSEMBLER__ + __LINKER__ + 0)
 COSMOPOLITAN_C_START_
 
