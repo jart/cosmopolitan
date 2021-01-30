39bb91fc44503f8e069534d1a141ae3d3b22dc87
Justine Tunney <jtunney@gmail.com>
2021-01-30 13:09:11 -0800

    Improve fcntl(F_{SET,GET}FL) implementation (#32)

--- test/libc/calls/fcntl_test.c
+++ test/libc/calls/fcntl_test.c
@@ -1,7 +1,7 @@
 /*-*- mode:c;indent-tabs-mode:nil;c-basic-offset:2;tab-width:8;coding:utf-8 -*-│
 │vi: set net ft=c ts=2 sts=2 sw=2 fenc=utf-8                                :vi│
 ╞══════════════════════════════════════════════════════════════════════════════╡
-│ Copyright 2020 Justine Alexandra Roberts Tunney                              │
+│ Copyright 2021 Justine Alexandra Roberts Tunney                              │
 │                                                                              │
 │ Permission to use, copy, modify, and/or distribute this software for         │
 │ any purpose with or without fee is hereby granted, provided that the         │
@@ -17,48 +17,52 @@
 │ PERFORMANCE OF THIS SOFTWARE.                                                │
 ╚─────────────────────────────────────────────────────────────────────────────*/
 #include "libc/calls/calls.h"
-#include "libc/calls/internal.h"
-#include "libc/nt/enum/accessmask.h"
-#include "libc/nt/enum/fileflagandattributes.h"
-#include "libc/nt/enum/filesharemode.h"
-#include "libc/nt/files.h"
+#include "libc/fmt/fmt.h"
+#include "libc/log/check.h"
+#include "libc/macros.h"
+#include "libc/runtime/runtime.h"
 #include "libc/sysv/consts/f.h"
-#include "libc/sysv/consts/fd.h"
 #include "libc/sysv/consts/o.h"
-#include "libc/sysv/errfuns.h"
+#include "libc/testlib/testlib.h"
+#include "libc/x/x.h"
 
-textwindows int fcntl$nt(int fd, int cmd, unsigned arg) {
-  uint32_t flags;
-  if (__isfdkind(fd, kFdFile) || __isfdkind(fd, kFdSocket)) {
-    switch (cmd) {
-      case F_GETFL:
-        return g_fds.p[fd].flags & (O_ACCMODE | O_APPEND | O_ASYNC | O_DIRECT |
-                                    O_NOATIME | O_NONBLOCK);
-      case F_SETFL:
-        /*
-         * - O_APPEND doesn't appear to be tunable at cursory glance
-         * - O_NONBLOCK might require we start doing all i/o in threads
-         * - O_DSYNC / O_RSYNC / O_SYNC maybe if we fsync() everything
-         */
-        return einval();
-      case F_GETFD:
-        if (g_fds.p[fd].flags & O_CLOEXEC) {
-          return FD_CLOEXEC;
-        } else {
-          return 0;
-        }
-      case F_SETFD:
-        if (arg & FD_CLOEXEC) {
-          g_fds.p[fd].flags |= O_CLOEXEC;
-          return FD_CLOEXEC;
-        } else {
-          g_fds.p[fd].flags &= ~O_CLOEXEC;
-          return 0;
-        }
-      default:
-        return einval();
-    }
-  } else {
-    return ebadf();
-  }
+char basedir[PATH_MAX];
+char testdir[PATH_MAX];
+
+void SetUp(void) {
+  getcwd(basedir, ARRAYLEN(basedir));
+  sprintf(testdir, "o/tmp/%s.%d", program_invocation_short_name, getpid());
+  makedirs(testdir, 0755);
+  CHECK_NE(-1, chdir(testdir));
+}
+
+void TearDown(void) {
+  CHECK_NE(-1, chdir(basedir));
+  CHECK_NE(-1, rmrf(testdir));
+}
+
+TEST(fcntl_getfl, testRemembersAccessMode) {
+  int fd;
+  ASSERT_NE(-1, (fd = open("foo", O_CREAT | O_RDWR, 0644)));
+  EXPECT_EQ(O_RDWR, fcntl(fd, F_GETFL) & O_ACCMODE);
+  EXPECT_NE(-1, close(fd));
+  ASSERT_NE(-1, (fd = open("foo", O_WRONLY, 0644)));
+  EXPECT_EQ(O_WRONLY, fcntl(fd, F_GETFL) & O_ACCMODE);
+  EXPECT_NE(-1, close(fd));
+}
+
+TEST(fcntl_setfl, testChangeAppendStatus) {
+  int fd;
+  char buf[8] = {0};
+  if (IsWindows()) return; /* doesn't appear possible on windows */
+  ASSERT_NE(-1, (fd = open("foo", O_CREAT | O_RDWR, 0644)));
+  EXPECT_EQ(3, write(fd, "foo", 3));
+  EXPECT_NE(-1, lseek(fd, 0, SEEK_SET));
+  EXPECT_NE(-1, fcntl(fd, F_SETFL, O_APPEND));
+  EXPECT_EQ(3, write(fd, "bar", 3));
+  EXPECT_NE(-1, lseek(fd, 0, SEEK_SET));
+  EXPECT_NE(-1, fcntl(fd, F_SETFL, 0));
+  EXPECT_EQ(6, read(fd, buf, 6));
+  EXPECT_STREQ("foobar", buf);
+  EXPECT_NE(-1, close(fd));
 }
