4651962070b14c3d70c44b5171cc8dfe5c3fc722
Justine Tunney <jtunney@gmail.com>
2021-03-02 01:04:18 -0800

    Limit frame size restriction to internal repo
    Fixes #91

--- build/definitions.mk
+++ build/definitions.mk
@@ -137,6 +137,7 @@ MATHEMATICAL =								\
 	-fwrapv
 
 DEFAULT_CPPFLAGS =							\
+	-DCOSMO								\
 	-DMODE='"$(MODE)"'						\
 	-DIMAGE_BASE_VIRTUAL=$(IMAGE_BASE_VIRTUAL)			\
 	-nostdinc							\
