6fa049c9c75b1eccf6bac3aacaa8fe88f68b504a
dosisod <39638017+dosisod@users.noreply.github.com>
2021-03-05 20:32:25 -0800

    Add macro for static_assert (#106)

--- libc/assert.h
+++ libc/assert.h
@@ -19,6 +19,8 @@ void __assert_fail(const char *, const char *, int) hidden wontreturn relegated;
     }                                           \
   } while (0)
 
+#define static_assert _Static_assert
+
 COSMOPOLITAN_C_END_
 #endif /* !(__ASSEMBLER__ + __LINKER__ + 0) */
 #endif /* COSMOPOLITAN_LIBC_ASSERT_H_ */
