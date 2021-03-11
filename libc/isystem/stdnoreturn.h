0f8adc258f763b752612fc5916c3045d88a36982
dosisod <39638017+dosisod@users.noreply.github.com>
2021-03-10 20:08:50 -0800

    Alias noreturn to wontreturn

--- libc/isystem/stdnoreturn.h
+++ libc/isystem/stdnoreturn.h
@@ -1,15 +1,10 @@
 #ifndef LIBC_ISYSTEM_STDNORETURN_H_
 #define LIBC_ISYSTEM_STDNORETURN_H_
 
-#if !(__ASSEMBLER__ + __LINKER__ + 0)
-#if __STDC_VERSION__ + 0 >= 201112
 COSMOPOLITAN_C_START_
 
-#define noreturn _Noreturn
+#define noreturn wontreturn
 
 COSMOPOLITAN_C_END_
-
-#endif /* C11 */
-#endif /* !(__ASSEMBLER__ + __LINKER__ + 0) */
 
 #endif
