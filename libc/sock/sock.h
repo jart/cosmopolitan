8279a993d50c7242c65d8dff4c05ad8d1fd5e9c9
Fabrizio Bertocci <fabriziobertocci@gmail.com>
2021-05-11 21:37:25 -0400

    Added some padding in place of ifru_map to ensure ifreq structure correctly match the structure defined in Linux

--- libc/sock/sock.h
+++ libc/sock/sock.h
@@ -110,24 +110,32 @@ struct ifreq {
     struct sockaddr ifru_addr;
     struct sockaddr ifru_dstaddr;
     struct sockaddr ifru_broadaddr;
-    short int ifru_flags;
-    int ifru_ivalue;
-    int ifru_mtu;
+    struct sockaddr ifru_netmask;
+    struct sockaddr ifru_hwaddr;
+    short           ifru_flags;
+    int	            ifru_ivalue;
+    int	            ifru_mtu;
+    char            ifru_map[24];   /* Unsupported, used as padding */
   } ifr_ifru;
 };
-# define ifr_name	ifr_ifrn.ifrn_name	/* interface name 	*/
-# define ifr_addr	ifr_ifru.ifru_addr	/* address		*/
-# define ifr_dstaddr	ifr_ifru.ifru_dstaddr	/* other end of p-p lnk	*/
-# define ifr_broadaddr	ifr_ifru.ifru_broadaddr	/* broadcast address	*/
-# define ifr_flags	ifr_ifru.ifru_flags	/* flags		*/
-# define ifr_metric	ifr_ifru.ifru_ivalue	/* metric		*/
-# define ifr_mtu	ifr_ifru.ifru_mtu	/* mtu			*/
-# define ifr_ifindex	ifr_ifru.ifru_ivalue    /* interface index      */
-# define ifr_bandwidth	ifr_ifru.ifru_ivalue	/* link bandwidth	*/
-# define ifr_qlen	ifr_ifru.ifru_ivalue	/* queue length		*/
-# define _IOT_ifreq	_IOT(_IOTS(char),IFNAMSIZ,_IOTS(char),16,0,0)
-# define _IOT_ifreq_short _IOT(_IOTS(char),IFNAMSIZ,_IOTS(short),1,0,0)
-# define _IOT_ifreq_int	_IOT(_IOTS(char),IFNAMSIZ,_IOTS(int),1,0,0)
+#define ifr_name	ifr_ifrn.ifrn_name	/* interface name 	*/
+#define ifr_addr	ifr_ifru.ifru_addr	/* address		*/
+#define ifr_dstaddr	ifr_ifru.ifru_dstaddr	/* other end of p-p lnk	*/
+#define ifr_broadaddr	ifr_ifru.ifru_broadaddr	/* broadcast address	*/
+#define	ifr_netmask	ifr_ifru.ifru_netmask	/* interface net mask	*/
+#define ifr_hwaddr	ifr_ifru.ifru_hwaddr	/* MAC address 		*/
+
+#define ifr_flags	ifr_ifru.ifru_flags	/* flags		*/
+#define ifr_metric	ifr_ifru.ifru_ivalue	/* metric		*/
+#define ifr_ifindex	ifr_ifru.ifru_ivalue    /* interface index      */
+#define ifr_bandwidth	ifr_ifru.ifru_ivalue	/* link bandwidth	*/
+#define ifr_qlen	ifr_ifru.ifru_ivalue	/* queue length		*/
+#define ifr_mtu	        ifr_ifru.ifru_mtu	/* mtu			*/
+/* ifru_map is unsupported  */
+
+#define _IOT_ifreq	_IOT(_IOTS(char),IFNAMSIZ,_IOTS(char),16,0,0)
+#define _IOT_ifreq_short _IOT(_IOTS(char),IFNAMSIZ,_IOTS(short),1,0,0)
+#define _IOT_ifreq_int	_IOT(_IOTS(char),IFNAMSIZ,_IOTS(int),1,0,0)
 
 #define IFF_UP  (1<<0)
 
