a8fb68af4b6407660afa05cea6bc10bba9fa1b51
Justine Tunney <jtunney@gmail.com>
2021-09-06 15:51:57 -0700

    Add bzip2 1.0.8

---
+++ third_party/bzip2/bzip2.mk
@@ -0,0 +1,57 @@
+#-*-mode:makefile-gmake;indent-tabs-mode:t;tab-width:8;coding:utf-8-*-┐
+#───vi: set et ft=make ts=8 tw=8 fenc=utf-8 :vi───────────────────────┘
+
+PKGS += THIRD_PARTY_BZIP2
+
+THIRD_PARTY_BZIP2_ARTIFACTS += THIRD_PARTY_BZIP2_A
+THIRD_PARTY_BZIP2 = $(THIRD_PARTY_BZIP2_A_DEPS) $(THIRD_PARTY_BZIP2_A)
+THIRD_PARTY_BZIP2_A = o/$(MODE)/third_party/bzip2/bzip2.a
+THIRD_PARTY_BZIP2_A_FILES := $(wildcard third_party/bzip2/*)
+THIRD_PARTY_BZIP2_A_HDRS = third_party/bzip2/bzip2.h
+THIRD_PARTY_BZIP2_A_HDRS_ALL = $(filter %.h,$(THIRD_PARTY_BZIP2_A_FILES))
+THIRD_PARTY_BZIP2_A_SRCS_S = $(filter %.S,$(THIRD_PARTY_BZIP2_A_FILES))
+THIRD_PARTY_BZIP2_A_SRCS_C = $(filter %.c,$(THIRD_PARTY_BZIP2_A_FILES))
+
+THIRD_PARTY_BZIP2_A_SRCS =				\
+	$(THIRD_PARTY_BZIP2_A_SRCS_S)			\
+	$(THIRD_PARTY_BZIP2_A_SRCS_C)
+
+THIRD_PARTY_BZIP2_A_OBJS =				\
+	$(THIRD_PARTY_BZIP2_A_SRCS_S:%.S=o/$(MODE)/%.o)	\
+	$(THIRD_PARTY_BZIP2_A_SRCS_C:%.c=o/$(MODE)/%.o)
+
+THIRD_PARTY_BZIP2_A_CHECKS =				\
+	$(THIRD_PARTY_BZIP2_A).pkg			\
+	$(THIRD_PARTY_BZIP2_A_HDRS_ALL:%=o/$(MODE)/%.ok)
+
+THIRD_PARTY_BZIP2_A_DIRECTDEPS =				\
+	LIBC_INTRIN					\
+	LIBC_NEXGEN32E					\
+	LIBC_STR					\
+	LIBC_STUBS
+
+THIRD_PARTY_BZIP2_A_DEPS :=				\
+	$(call uniq,$(foreach x,$(THIRD_PARTY_BZIP2_A_DIRECTDEPS),$($(x))))
+
+$(THIRD_PARTY_BZIP2_A):					\
+		third_party/bzip2/			\
+		$(THIRD_PARTY_BZIP2_A).pkg		\
+		$(THIRD_PARTY_BZIP2_A_OBJS)
+
+$(THIRD_PARTY_BZIP2_A).pkg:				\
+		$(THIRD_PARTY_BZIP2_A_OBJS)		\
+		$(foreach x,$(THIRD_PARTY_BZIP2_A_DIRECTDEPS),$($(x)_A).pkg)
+
+THIRD_PARTY_BZIP2_LIBS = $(foreach x,$(THIRD_PARTY_BZIP2_ARTIFACTS),$($(x)))
+THIRD_PARTY_BZIP2_SRCS = $(foreach x,$(THIRD_PARTY_BZIP2_ARTIFACTS),$($(x)_SRCS))
+THIRD_PARTY_BZIP2_HDRS = $(foreach x,$(THIRD_PARTY_BZIP2_ARTIFACTS),$($(x)_HDRS))
+THIRD_PARTY_BZIP2_HDRS_ALL = $(foreach x,$(THIRD_PARTY_BZIP2_ARTIFACTS),$($(x)_HDRS_ALL))
+THIRD_PARTY_BZIP2_BINS = $(foreach x,$(THIRD_PARTY_BZIP2_ARTIFACTS),$($(x)_BINS))
+THIRD_PARTY_BZIP2_CHECKS = $(foreach x,$(THIRD_PARTY_BZIP2_ARTIFACTS),$($(x)_CHECKS))
+THIRD_PARTY_BZIP2_OBJS = $(foreach x,$(THIRD_PARTY_BZIP2_ARTIFACTS),$($(x)_OBJS))
+$(THIRD_PARTY_BZIP2_OBJS): $(BUILD_FILES) third_party/bzip2/bzip2.mk
+
+.PHONY: o/$(MODE)/third_party/bzip2
+o/$(MODE)/third_party/bzip2:				\
+		$(THIRD_PARTY_BZIP2_A)			\
+		$(THIRD_PARTY_BZIP2_CHECKS)
