cf3174dc74acd90c986630eb9357dd40dd065dfe
Justine Tunney <jtunney@gmail.com>
2022-04-22 19:14:49 -0700

    Put redbean lua init demo code

--- tool/net/demo/.init.lua
+++ tool/net/demo/.init.lua
@@ -20,17 +20,17 @@ db:exec[[
   INSERT INTO test (content) VALUES ('Hello Sqlite3');
 ]]
 
--- -- this intercepts all requests if it's defined
--- function OnHttpRequest()
---    if HasParam('magic') then
---       Write('<p>\r\n')
---       Write('OnHttpRequest() has intercepted your request<br>\r\n')
---       Write('because you specified the magic parameter\r\n')
---       Write('<pre>\r\n')
---       Write(EscapeHtml(LoadAsset('/.init.lua')))
---       Write('</pre>\r\n')
---    else
---       Route() -- this asks redbean to do the default thing
---    end
---    SetHeader('Server', 'redbean!')
--- end
+-- this intercepts all requests if it's defined
+function OnHttpRequest()
+   if HasParam('magic') then
+      Write('<p>\r\n')
+      Write('OnHttpRequest() has intercepted your request<br>\r\n')
+      Write('because you specified the magic parameter\r\n')
+      Write('<pre>\r\n')
+      Write(EscapeHtml(LoadAsset('/.init.lua')))
+      Write('</pre>\r\n')
+   else
+      Route() -- this asks redbean to do the default thing
+   end
+   SetHeader('Server', 'redbean!')
+end
