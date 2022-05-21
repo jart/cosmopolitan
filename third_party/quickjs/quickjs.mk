96684435cfd515b3c238d560db1659237377bef5
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2022-05-21 17:46:31 -0500

    extend quotas for various slow tests

--- third_party/quickjs/quickjs.mk
+++ third_party/quickjs/quickjs.mk
@@ -205,8 +205,8 @@ o/$(MODE)/third_party/quickjs/quickjs.o:					\
 		OVERRIDE_CPPFLAGS +=						\
 			-DSTACK_FRAME_UNLIMITED
 
-o/$(MODE)/third_party/quickjs/call.o: QUOTA = -M1024m -C32
-o/$(MODE)/third_party/quickjs/quickjs.o: QUOTA = -M512m -C32
+o/$(MODE)/third_party/quickjs/call.o: QUOTA = -M1024m -C32 -L180
+o/$(MODE)/third_party/quickjs/quickjs.o: QUOTA = -M512m -C32 -L180
 
 .PHONY: o/$(MODE)/third_party/quickjs
 o/$(MODE)/third_party/quickjs:							\
