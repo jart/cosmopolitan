3d07f362d7600a0aefca10da067d1744091de965
Justine Tunney <jtunney@gmail.com>
2022-06-23 16:09:20 -0700

    Release redbean 2.0.5

--- tool/net/redbean.c
+++ tool/net/redbean.c
@@ -143,7 +143,7 @@ STATIC_YOINK("ShowCrashReportsEarly");
 #define REDBEAN "redbean"
 #endif
 
-#define VERSION          0x020004
+#define VERSION          0x020005
 #define HEARTBEAT        5000 /*ms*/
 #define HASH_LOAD_FACTOR /* 1. / */ 4
 #define MONITOR_MICROS   150000
