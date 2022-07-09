jart/cosmopolitan

commit 85aecbda676c59d38a7ca266386871ccb541a2d1

ttyraster: reset ANSI attributes after each line (#469)

dsp/tty/ttyraster.c
@@ -775,7 +775,7 @@ char *ttyraster(char *v, const struct TtyRgb *c, size_t yn, size_t xn,
   struct Glyph glyph;
   struct TtyRgb chun[4], lastchunk[4];
   for (y = 0; y < yn; y += 2, c += xn) {
-    if (y) *v++ = '\r', *v++ = '\n';
+    if (y) v = stpcpy(v, "\e[0m\r\n");
     for (x = 0; x < xn; x += 2, c += 2) {
       CopyChunk(chun, c, xn);
       if (ttyquant()->alg == kTtyQuantTrue) {
@@ -795,6 +795,6 @@ char *ttyraster(char *v, const struct TtyRgb *c, size_t yn, size_t xn,
       memcpy(lastchunk, chun, sizeof(chun));
     }
   }
-  *v = '\0';
+  v = stpcpy(v, "\e[0m");
   return v;
 }

test/dsp/tty/ttyraster_test.c
@@ -43,7 +43,7 @@ void ttyraster2x2_true(void) {
 TEST(ttyraster, testCorner) {
   ttyraster_true_setup();
   ttyraster2x2_true();
-  EXPECT_STREQ("\e[48;2;139;0;0;38;2;3;3;3m▝", vtbuf);
+  EXPECT_STREQ("\e[48;2;139;0;0;38;2;3;3;3m▝\e[0m", vtbuf);
 }
 
 TEST(ttyraster, testFullBlock_favorsSpace) {
@@ -54,7 +54,7 @@ TEST(ttyraster, testFullBlock_favorsSpace) {
                 {DARKRED, DARKRED},
             },
             2, 2, kBlack, kBlack);
-  EXPECT_STREQ("\e[48;2;139;0;0m ", vtbuf);
+  EXPECT_STREQ("\e[48;2;139;0;0m \e[0m", vtbuf);
 }
 
 TEST(ttyraster, testFullBlock_favorsUnicodeWhenCurrenttFgMatchesButNotBg) {
@@ -65,7 +65,7 @@ TEST(ttyraster, testFullBlock_favorsUnicodeWhenCurrenttFgMatchesButNotBg) {
                 {DARKRED, DARKRED, GRAY1, GRAY1},
             },
             2, 4, kBlack, kBlack);
-  EXPECT_STREQ("\e[48;2;139;0;0;38;2;3;3;3m▝█", vtbuf);
+  EXPECT_STREQ("\e[48;2;139;0;0;38;2;3;3;3m▝█\e[0m", vtbuf);
 }
 
 TEST(ttyraster, testFullBlock_forcesSwitchBackToSpaceForRuns) {
@@ -76,7 +76,7 @@ TEST(ttyraster, testFullBlock_forcesSwitchBackToSpaceForRuns) {
                 {DARKRED, DARKRED, GRAY1, GRAY1, GRAY1, GRAY1, GRAY1, GRAY1},
             },
             2, 8, kBlack, kBlack);
-  EXPECT_STREQ("\e[48;2;139;0;0;38;2;3;3;3m▝█\e[48;2;3;3;3m  ", vtbuf);
+  EXPECT_STREQ("\e[48;2;139;0;0;38;2;3;3;3m▝█\e[48;2;3;3;3m  \e[0m", vtbuf);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -89,7 +89,7 @@ TEST(ttyraster_cp437, testSide) {
                 {DARKRED, GRAY1},
             },
             2, 2, kBlack, kBlack);
-  EXPECT_STREQ("\e[48;2;3;3;3;38;2;139;0;0m▌", vtbuf);
+  EXPECT_STREQ("\e[48;2;3;3;3;38;2;139;0;0m▌\e[0m", vtbuf);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -110,7 +110,7 @@ void ttyraster2x2_xterm256(void) {
 TEST(ttyraster_xterm256, testCorner) {
   ttyraster_xterm256_setup();
   ttyraster2x2_xterm256();
-  EXPECT_STREQ("\e[48;5;16;38;5;231m▝", vtbuf);
+  EXPECT_STREQ("\e[48;5;16;38;5;231m▝\e[0m", vtbuf);
 }
 
 void ttyraster6x2_xterm256(void) {
@@ -125,7 +125,7 @@ void ttyraster6x2_xterm256(void) {
 TEST(ttyraster_xterm256, testCornerRepeat3) {
   ttyraster_xterm256_setup();
   ttyraster6x2_xterm256();
-  EXPECT_STREQ("\e[48;5;16;38;5;231m▝▝▝", vtbuf);
+  EXPECT_STREQ("\e[48;5;16;38;5;231m▝▝▝\e[0m", vtbuf);
 }
 
 ////////////////////////////////////////////////////////////////////////////////

commit f2a6a690416de98e283ba2409a7acf8d5c29378c

ttyraster: restore colors at start of each line
The patch in #469 was buggy for images where the start of a row matched
the end of the previous row. We don't re-issue the `setbgfg` ANSI codes
when we think that the color hasn't changed. But by sending an `\e[0m`
sequence at the end of the line without updating `bg` or `fg`, we
desynced `bg` and `fg` from the actual ANSI state. Now, we simply follow
that line-terminating `\e[0m` with another `setbgfg` call.

This bug was visible in images with a constant-color matte border:

![Screenshot of `printimage` output before and after this commit when
run on `lemurs.png` and `lemursborder.png`, where `lemursborder.png` has
a thick blue border around the outside of the image. Both versions look
fine for `lemurs.png`. For `lemursborder.png`, the "before" version has
a chess board pattern for the left border (except for the first row) and
the bottom border. The "after" version looks correct.][ss]

[ss]: https://user-images.githubusercontent.com/4317806/178120511-c1b89348-2376-4bf2-a2d3-8723d2663bd4.png

Fixes: 85aecbda676c ("ttyraster: reset ANSI attributes after each line (#469)")
wchargin-branch: ttyraster-restore-each-line
wchargin-source: 621a788cfa0a87ce360e142a6004e325cca70caa

dsp/tty/ttyraster.c
@@ -775,7 +775,10 @@ char *ttyraster(char *v, const struct TtyRgb *c, size_t yn, size_t xn,
   struct Glyph glyph;
   struct TtyRgb chun[4], lastchunk[4];
   for (y = 0; y < yn; y += 2, c += xn) {
-    if (y) v = stpcpy(v, "\e[0m\r\n");
+    if (y) {
+      v = stpcpy(v, "\e[0m\r\n");
+      v = setbgfg(v, bg, fg);
+    }
     for (x = 0; x < xn; x += 2, c += 2) {
       CopyChunk(chun, c, xn);
       if (ttyquant()->alg == kTtyQuantTrue) {