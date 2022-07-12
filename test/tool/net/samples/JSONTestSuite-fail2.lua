3c10fb55807393ac6d22a247038871e1386f0b80
Gautham <41098605+ahgamut@users.noreply.github.com>
2022-07-12 06:34:51 +0530

    Fix quote escapes in JSON tests (#481)

--- test/tool/net/samples/JSONTestSuite-fail2.lua
+++ test/tool/net/samples/JSONTestSuite-fail2.lua
@@ -32,127 +32,125 @@
 -- ljson should reject all of them as invalid
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/n_structure_whitespace_formfeed.json
- -- (converted to binary for safety)
-assert(false == pcall(DecodeJson, [[ \x5b\xc\x5d ]]))
+-- (converted to binary for safety)
+assert(false == pcall(DecodeJson, ' \x5b\x0c\x5d '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/n_structure_whitespace_U+2060_word_joiner.json
- -- (converted to binary for safety)
-assert(false == pcall(DecodeJson, [[ \x5b\xe2\x81\xa0\x5d ]]))
+-- (converted to binary for safety)
+assert(false == pcall(DecodeJson, ' \x5b\xe2\x81\xa0\x5d '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/n_structure_unicode-identifier.json
- -- (converted to binary for safety)
-assert(false == pcall(DecodeJson, [[ \xc3\xa5 ]]))
+-- (converted to binary for safety)
+assert(false == pcall(DecodeJson, ' \xc3\xa5 '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/n_structure_unclosed_object.json
-assert(false == pcall(DecodeJson, [[ {"asd":"asd" ]]))
+assert(false == pcall(DecodeJson, ' {"asd":"asd" '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/n_structure_unclosed_array_unfinished_true.json
-assert(false == pcall(DecodeJson, [[ [ false, tru ]]))
+assert(false == pcall(DecodeJson, ' [ false, tru '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/n_structure_unclosed_array_unfinished_false.json
-assert(false == pcall(DecodeJson, [[ [ true, fals ]]))
+assert(false == pcall(DecodeJson, ' [ true, fals '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/n_structure_unclosed_array_partial_null.json
-assert(false == pcall(DecodeJson, [[ [ false, nul ]]))
+assert(false == pcall(DecodeJson, ' [ false, nul '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/n_structure_unclosed_array.json
-assert(false == pcall(DecodeJson, [[ [1 ]]))
+assert(false == pcall(DecodeJson, ' [1 '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/n_structure_uescaped_LF_before_string.json
- -- (converted to binary for safety)
-assert(false == pcall(DecodeJson, [[ \x5b\x5c\x75\x30\x30\x30\x41\x22\x22\x5d ]]))
+-- (converted to binary for safety)
+assert(false == pcall(DecodeJson, ' \x5b\x5c\x75\x30\x30\x30\x41\x22\x22\x5d '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/n_structure_single_star.json
-assert(false == pcall(DecodeJson, [[ * ]]))
+assert(false == pcall(DecodeJson, ' * '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/n_structure_single_eacute.json
-assert(false == pcall(DecodeJson, [[ é ]]))
+assert(false == pcall(DecodeJson, ' é '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/n_structure_open_open.json
-assert(false == pcall(DecodeJson, [[ ["\{["\{["\{["\{ ]]))
+assert(false == pcall(DecodeJson, [[ ["{["{["{["{ ]]))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/n_structure_open_object_string_with_apostrophes.json
- -- (converted to binary for safety)
-assert(false == pcall(DecodeJson, [[ \x7b\x27\x61\x27 ]]))
+-- (converted to binary for safety)
+assert(false == pcall(DecodeJson, ' \x7b\x27\x61\x27 '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/n_structure_open_object_open_string.json
- -- (converted to binary for safety)
-assert(false == pcall(DecodeJson, [[ \x7b\x22\x61 ]]))
+-- (converted to binary for safety)
+assert(false == pcall(DecodeJson, ' \x7b\x22\x61 '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/n_structure_open_object_open_array.json
-assert(false == pcall(DecodeJson, [[ {[ ]]))
+assert(false == pcall(DecodeJson, ' {[ '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/n_structure_open_object_comma.json
-assert(false == pcall(DecodeJson, [[ {, ]]))
+assert(false == pcall(DecodeJson, ' {, '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/n_structure_open_object_close_array.json
-assert(false == pcall(DecodeJson, [[ {] ]]))
+assert(false == pcall(DecodeJson, ' {] '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/n_structure_open_object.json
-assert(false == pcall(DecodeJson, [[ { ]]))
+assert(false == pcall(DecodeJson, ' { '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/n_structure_open_array_string.json
- -- (converted to binary for safety)
-assert(false == pcall(DecodeJson, [[ \x5b\x22\x61\x22 ]]))
+-- (converted to binary for safety)
+assert(false == pcall(DecodeJson, ' \x5b\x22\x61\x22 '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/n_structure_open_array_open_string.json
- -- (converted to binary for safety)
-assert(false == pcall(DecodeJson, [[ \x5b\x22\x61 ]]))
+-- (converted to binary for safety)
+assert(false == pcall(DecodeJson, ' \x5b\x22\x61 '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/n_structure_open_array_open_object.json
-assert(false == pcall(DecodeJson, [[ [{ ]]))
+assert(false == pcall(DecodeJson, ' [{ '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/n_structure_open_array_comma.json
-assert(false == pcall(DecodeJson, [[ [, ]]))
+assert(false == pcall(DecodeJson, ' [, '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/n_structure_open_array_apostrophe.json
-assert(false == pcall(DecodeJson, [[ [' ]]))
+assert(false == pcall(DecodeJson, ' [\' '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/n_structure_object_with_comment.json
-assert(false == pcall(DecodeJson, [[ {"a":/*comment*/"b"} ]]))
+assert(false == pcall(DecodeJson, ' {"a":/*comment*/"b"} '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/n_structure_object_unclosed_no_value.json
-assert(false == pcall(DecodeJson, [[ {"": ]]))
+assert(false == pcall(DecodeJson, ' {"": '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/n_structure_null-byte-outside-string.json
- -- (converted to binary for safety)
-assert(false == pcall(DecodeJson, [[ \x5b\x0\x5d ]]))
+-- (converted to binary for safety)
+assert(false == pcall(DecodeJson, ' \x5b\x00\x5d '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/n_structure_lone-open-bracket.json
-assert(false == pcall(DecodeJson, [[ [ ]]))
+assert(false == pcall(DecodeJson, ' [ '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/n_structure_lone-invalid-utf-8.json
- -- (converted to binary for safety)
-assert(false == pcall(DecodeJson, [[ \xe5 ]]))
+-- (converted to binary for safety)
+assert(false == pcall(DecodeJson, ' \xe5 '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/n_structure_incomplete_UTF8_BOM.json
-assert(false == pcall(DecodeJson, [[ ï»{} ]]))
+assert(false == pcall(DecodeJson, ' ï»{} '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/n_structure_comma_instead_of_closing_brace.json
-assert(false == pcall(DecodeJson, [[ {"x": true, ]]))
+assert(false == pcall(DecodeJson, ' {"x": true, '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/n_structure_capitalized_True.json
-assert(false == pcall(DecodeJson, [[ [True] ]]))
+assert(false == pcall(DecodeJson, ' [True] '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/n_structure_ascii-unicode-identifier.json
- -- (converted to binary for safety)
-assert(false == pcall(DecodeJson, [[ \x61\xc3\xa5 ]]))
+-- (converted to binary for safety)
+assert(false == pcall(DecodeJson, ' \x61\xc3\xa5 '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/n_structure_array_with_unclosed_string.json
- -- (converted to binary for safety)
-assert(false == pcall(DecodeJson, [[ \x5b\x22\x61\x73\x64\x5d ]]))
+-- (converted to binary for safety)
+assert(false == pcall(DecodeJson, ' \x5b\x22\x61\x73\x64\x5d '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/n_structure_angle_bracket_null.json
-assert(false == pcall(DecodeJson, [[ [<null>] ]]))
+assert(false == pcall(DecodeJson, ' [<null>] '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/n_structure_angle_bracket_..json
-assert(false == pcall(DecodeJson, [[ <.> ]]))
+assert(false == pcall(DecodeJson, ' <.> '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/n_structure_UTF8_BOM_no_data.json
-assert(false == pcall(DecodeJson, [[ ï»¿ ]]))
+assert(false == pcall(DecodeJson, ' ï»¿ '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/n_structure_U+2060_word_joined.json
- -- (converted to binary for safety)
-assert(false == pcall(DecodeJson, [[ \x5b\xe2\x81\xa0\x5d ]]))
-
-
+-- (converted to binary for safety)
+assert(false == pcall(DecodeJson, ' \x5b\xe2\x81\xa0\x5d '))
--- test/tool/net/samples/JSONTestSuite-okay.lua
+++ test/tool/net/samples/JSONTestSuite-okay.lua
@@ -34,7 +34,7 @@
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/i_structure_UTF-8_BOM_empty_object.json
 -- (converted to binary for safety)
-assert(nil ~= pcall(DecodeJson, [[ \xef\xbb\xbf{} ]]))
+assert(nil ~= pcall(DecodeJson, ' \xef\xbb\xbf{} '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/i_structure_500_nested_arrays.json
 -- (added spaces between [[ and ]] so lua doesn't get confused)
@@ -67,124 +67,123 @@ assert(nil ~= pcall(DecodeJson, [[ [ [ [ [ [ [ [ [ [ [ [ [ [ [ [ [ [ [ [ [ [ [
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/i_string_utf16LE_no_BOM.json
 -- (converted to binary for safety)
-assert(nil ~= pcall(DecodeJson, [[ \x5b\x0\x22\x0\xe9\x0\x22\x0\x5d\x0 ]]))
+assert(nil ~= pcall(DecodeJson, ' \x5b\x00\x22\x00\xe9\x00\x22\x00\x5d\x00 '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/i_string_utf16BE_no_BOM.json
 -- (converted to binary for safety)
-assert(nil ~= pcall(DecodeJson, [[ \x0\x5b\x0\x22\x0\xe9\x0\x22\x0\x5d ]]))
+assert(nil ~= pcall(DecodeJson, ' \x00\x5b\x00\x22\x00\xe9\x00\x22\x00\x5d '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/i_string_truncated-utf-8.json
 -- (converted to binary for safety)
-assert(nil ~= pcall(DecodeJson, [[ \x5b\x22\xe0\xff\x22\x5d ]]))
+assert(nil ~= pcall(DecodeJson, ' \x5b\x22\xe0\xff\x22\x5d '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/i_string_overlong_sequence_6_bytes_null.json
 -- (converted to binary for safety)
-assert(nil ~= pcall(DecodeJson, [[ \x5b\x22\xfc\x80\x80\x80\x80\x80\x22\x5d ]]))
+assert(nil ~= pcall(DecodeJson, ' \x5b\x22\xfc\x80\x80\x80\x80\x80\x22\x5d '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/i_string_overlong_sequence_6_bytes.json
 -- (converted to binary for safety)
-assert(nil ~= pcall(DecodeJson, [[ \x5b\x22\xfc\x83\xbf\xbf\xbf\xbf\x22\x5d ]]))
+assert(nil ~= pcall(DecodeJson, ' \x5b\x22\xfc\x83\xbf\xbf\xbf\xbf\x22\x5d '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/i_string_overlong_sequence_2_bytes.json
 -- (converted to binary for safety)
-assert(nil ~= pcall(DecodeJson, [[ \x5b\x22\xc0\xaf\x22\x5d ]]))
+assert(nil ~= pcall(DecodeJson, ' \x5b\x22\xc0\xaf\x22\x5d '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/i_string_not_in_unicode_range.json
 -- (converted to binary for safety)
-assert(nil ~= pcall(DecodeJson, [[ \x5b\x22\xf4\xbf\xbf\xbf\x22\x5d ]]))
+assert(nil ~= pcall(DecodeJson, ' \x5b\x22\xf4\xbf\xbf\xbf\x22\x5d '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/i_string_lone_utf8_continuation_byte.json
 -- (converted to binary for safety)
-assert(nil ~= pcall(DecodeJson, [[ \x5b\x22\x81\x22\x5d ]]))
+assert(nil ~= pcall(DecodeJson, ' \x5b\x22\x81\x22\x5d '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/i_string_lone_second_surrogate.json
 -- (converted to binary for safety)
-assert(nil ~= pcall(DecodeJson, [[ \x5b\x22\x5c\x75\x44\x46\x41\x41\x22\x5d ]]))
+assert(nil ~= pcall(DecodeJson, ' \x5b\x22\x5c\x75\x44\x46\x41\x41\x22\x5d '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/i_string_iso_latin_1.json
 -- (converted to binary for safety)
-assert(nil ~= pcall(DecodeJson, [[ \x5b\x22\xe9\x22\x5d ]]))
+assert(nil ~= pcall(DecodeJson, ' \x5b\x22\xe9\x22\x5d '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/i_string_inverted_surrogates_U+1D11E.json
 -- (converted to binary for safety)
-assert(nil ~= pcall(DecodeJson, [[ \x5b\x22\x5c\x75\x44\x64\x31\x65\x5c\x75\x44\x38\x33\x34\x22\x5d ]]))
+assert(nil ~= pcall(DecodeJson, ' \x5b\x22\x5c\x75\x44\x64\x31\x65\x5c\x75\x44\x38\x33\x34\x22\x5d '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/i_string_invalid_utf-8.json
 -- (converted to binary for safety)
-assert(nil ~= pcall(DecodeJson, [[ \x5b\x22\xff\x22\x5d ]]))
+assert(nil ~= pcall(DecodeJson, ' \x5b\x22\xff\x22\x5d '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/i_string_invalid_surrogate.json
 -- (converted to binary for safety)
-assert(nil ~= pcall(DecodeJson, [[ \x5b\x22\x5c\x75\x64\x38\x30\x30\x61\x62\x63\x22\x5d ]]))
+assert(nil ~= pcall(DecodeJson, ' \x5b\x22\x5c\x75\x64\x38\x30\x30\x61\x62\x63\x22\x5d '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/i_string_invalid_lonely_surrogate.json
 -- (converted to binary for safety)
-assert(nil ~= pcall(DecodeJson, [[ \x5b\x22\x5c\x75\x64\x38\x30\x30\x22\x5d ]]))
+assert(nil ~= pcall(DecodeJson, ' \x5b\x22\x5c\x75\x64\x38\x30\x30\x22\x5d '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/i_string_incomplete_surrogates_escape_valid.json
 -- (converted to binary for safety)
-assert(nil ~= pcall(DecodeJson, [[ \x5b\x22\x5c\x75\x44\x38\x30\x30\x5c\x75\x44\x38\x30\x30\x5c\x6e\x22\x5d ]]))
+assert(nil ~= pcall(DecodeJson, ' \x5b\x22\x5c\x75\x44\x38\x30\x30\x5c\x75\x44\x38\x30\x30\x5c\x6e\x22\x5d '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/i_string_incomplete_surrogate_pair.json
 -- (converted to binary for safety)
-assert(nil ~= pcall(DecodeJson, [[ \x5b\x22\x5c\x75\x44\x64\x31\x65\x61\x22\x5d ]]))
+assert(nil ~= pcall(DecodeJson, ' \x5b\x22\x5c\x75\x44\x64\x31\x65\x61\x22\x5d '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/i_string_incomplete_surrogate_and_escape_valid.json
 -- (converted to binary for safety)
-assert(nil ~= pcall(DecodeJson, [[ \x5b\x22\x5c\x75\x44\x38\x30\x30\x5c\x6e\x22\x5d ]]))
+assert(nil ~= pcall(DecodeJson, ' \x5b\x22\x5c\x75\x44\x38\x30\x30\x5c\x6e\x22\x5d '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/i_string_UTF8_surrogate_U+D800.json
 -- (converted to binary for safety)
-assert(nil ~= pcall(DecodeJson, [[ \x5b\x22\xed\xa0\x80\x22\x5d ]]))
+assert(nil ~= pcall(DecodeJson, ' \x5b\x22\xed\xa0\x80\x22\x5d '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/i_string_UTF-8_invalid_sequence.json
 -- (converted to binary for safety)
-assert(nil ~= pcall(DecodeJson, [[ \x5b\x22\xe6\x97\xa5\xd1\x88\xfa\x22\x5d ]]))
+assert(nil ~= pcall(DecodeJson, ' \x5b\x22\xe6\x97\xa5\xd1\x88\xfa\x22\x5d '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/i_string_UTF-16LE_with_BOM.json
 -- (converted to binary for safety)
-assert(nil ~= pcall(DecodeJson, [[ \xff\xfe\x5b\x0\x22\x0\xe9\x0\x22\x0\x5d\x0 ]]))
+assert(nil ~= pcall(DecodeJson, ' \xff\xfe\x5b\x00\x22\x00\xe9\x00\x22\x00\x5d\x00 '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/i_string_1st_valid_surrogate_2nd_invalid.json
 -- (converted to binary for safety)
-assert(nil ~= pcall(DecodeJson, [[ \x5b\x22\x5c\x75\x44\x38\x38\x38\x5c\x75\x31\x32\x33\x34\x22\x5d ]]))
+assert(nil ~= pcall(DecodeJson, ' \x5b\x22\x5c\x75\x44\x38\x38\x38\x5c\x75\x31\x32\x33\x34\x22\x5d '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/i_string_1st_surrogate_but_2nd_missing.json
 -- (converted to binary for safety)
-assert(nil ~= pcall(DecodeJson, [[ \x5b\x22\x5c\x75\x44\x41\x44\x41\x22\x5d ]]))
+assert(nil ~= pcall(DecodeJson, ' \x5b\x22\x5c\x75\x44\x41\x44\x41\x22\x5d '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/i_object_key_lone_2nd_surrogate.json
 -- (converted to binary for safety)
-assert(nil ~= pcall(DecodeJson, [[  \x7b\x22\x5c\x75\x44\x46\x41\x41\x22\x3a\x30\x7d ]]))
+assert(nil ~= pcall(DecodeJson, '  \x7b\x22\x5c\x75\x44\x46\x41\x41\x22\x3a\x30\x7d '))
 
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/i_number_very_big_negative_int.json
-assert(nil ~= pcall(DecodeJson, [[ [-237462374673276894279832749832423479823246327846] ]]))
+assert(nil ~= pcall(DecodeJson, ' [-237462374673276894279832749832423479823246327846] '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/i_number_too_big_pos_int.json
-assert(nil ~= pcall(DecodeJson, [[ [100000000000000000000] ]]))
+assert(nil ~= pcall(DecodeJson, ' [100000000000000000000] '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/i_number_too_big_neg_int.json
-assert(nil ~= pcall(DecodeJson, [[ [-123123123123123123123123123123] ]]))
+assert(nil ~= pcall(DecodeJson, ' [-123123123123123123123123123123] '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/i_number_real_underflow.json
-assert(nil ~= pcall(DecodeJson, [[ [123e-10000000] ]]))
+assert(nil ~= pcall(DecodeJson, ' [123e-10000000] '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/i_number_real_pos_overflow.json
-assert(nil ~= pcall(DecodeJson, [[ [123123e100000] ]]))
+assert(nil ~= pcall(DecodeJson, ' [123123e100000] '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/i_number_real_neg_overflow.json
-assert(nil ~= pcall(DecodeJson, [[ [-123123e100000] ]]))
+assert(nil ~= pcall(DecodeJson, ' [-123123e100000] '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/i_number_pos_double_huge_exp.json
-assert(nil ~= pcall(DecodeJson, [[ [1.5e+9999] ]]))
+assert(nil ~= pcall(DecodeJson, ' [1.5e+9999] '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/i_number_neg_int_huge_exp.json
-assert(nil ~= pcall(DecodeJson, [[ [-1e+9999] ]]))
+assert(nil ~= pcall(DecodeJson, ' [-1e+9999] '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/i_number_huge_exp.json
-assert(nil ~= pcall(DecodeJson, [[ [0.4e00669999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999969999999006] ]]))
+assert(nil ~= pcall(DecodeJson, ' [0.4e00669999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999969999999006] '))
 
 -- https://github.com/nst/JSONTestSuite/tree/d64aefb55228d9584d3e5b2433f720ea8fd00c82/test_parsing/i_number_double_huge_neg_exp.json
-assert(nil ~= pcall(DecodeJson, [[ [123.456e-789] ]]))
-
+assert(nil ~= pcall(DecodeJson, ' [123.456e-789] '))
