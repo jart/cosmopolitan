81201b793cc93c54d97194d4077c224b0b73b83a
Your Name <youremail@yourdomain.com>
2022-08-03 10:58:45 +0200

    generative svg walkthrough

---
+++ tool/net/demo/gensvg/4/index.lua
@@ -0,0 +1,71 @@
+local top = [[
+    <!DOCTYPE html>
+    <html lang=en>
+    
+    <head>
+        <link rel="icon" href="data:;base64,iVBORw0KGgo=">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="description">
+        <style>
+        * {margin: 0;padding: 0;box-sizing: border-box;}
+        body {margin: 0 auto;font-family: Roboto, Helvetica, Arial, sans-serif;color: #444444;line-height: 1;max-width: 960px;padding: 30px;}
+        h1, h2, h3, h4 {color: #111111;font-weight: 400;}
+        h1, h2, h3, h4, h5, p {margin-bottom: 24px;padding: 0;}
+        h1 {font-size: 48px;}
+        h2 {font-size: 36px;margin: 24px 0 6px;}
+        .button {min-height:1rem;border: none;border-radius: 0.25rem;background: #1E88E5;color: white;font-family: sans-serif;font-size: 1rem;line-height: 1.2;white-space: nowrap;text-decoration: none;padding: 0.25rem 0.5rem;margin: 0.25rem;cursor: pointer;}
+        body {height: 100vh;display: grid;place-items: center;background: hsl(0, 0%, 93%);}
+        .canvas {width: 75vmin;height: 75vmin;background: hsl(350, 70%, 80%);}
+        .shadow {filter: drop-shadow(1px 1px 0.5px rgba(0, 0, 0, .25));}
+        </style>
+    </head>
+    
+    <body>
+]]
+
+local bottom = [[
+    <a href="https://github.com/jart/cosmopolitan/tree/master/tool/net/demo/gensvg/4/index.lua">Show The Code</a>
+    </body>
+</html>
+]]
+
+local nrows = 10
+local ncols = 10
+local sqsize = 20
+local seed = math.random()
+
+local function rect(x, y, w, h, rx)
+    return string.format('<rect x="%s" y="%s" width="%s" height="%s" rx="%s">',
+                         x, y, w, h, math.random(5,12))
+end
+
+local function writeBlock(i, j)
+    Write('<g class="draw-block">')
+    Write(rect(i * sqsize - sqsize, j * sqsize - sqsize, sqsize, sqsize))
+    Write('</g>')
+end
+
+local function render()
+    local xsize = nrows * sqsize
+    local ysize = ncols * sqsize
+    local style = string.format('style="background-color: hsl(%s,90%%,90%%);"',
+                                seed * 360)
+    Write('<svg class="canvas" ' .. style .. ' viewBox="0 0 ' .. xsize .. ' ' ..
+              ysize .. '">')
+    for i = 1, nrows, 1 do for j = 1, ncols, 1 do writeBlock(i, j) end end
+    Write('</svg>')
+end
+
+Write(top)
+Write("<h2>Randomize Plumpness</h2>")
+render()
+Write('<code>')
+Write(EscapeHtml(
+          [[string.format('<rect x="%s" y="%s" width="%s" height="%s" rx="%s">', x, y, w, h, math.random(5,15))]]))
+Write('</code>')
+Write('<div>')
+Write('<a class="button" href="/gensvg/3">Back</a>')
+Write('<a class="button" href="/gensvg/5">Next</a>')
+Write('</div>')
+Write(bottom)
