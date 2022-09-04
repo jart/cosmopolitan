9c017c98d36a151dbcc67db3ed7801a59e71dfce
Gavin Hayes <gavin@computoid.com>
2022-09-04 03:10:17 -0400

    Add sysconf to unistd.h (#575)

--- libc/isystem/unistd.h
+++ libc/isystem/unistd.h
@@ -2,6 +2,7 @@
 #define LIBC_ISYSTEM_UNISTD_H_
 #include "libc/calls/calls.h"
 #include "libc/calls/weirdtypes.h"
+#include "libc/runtime/sysconf.h"
 #include "libc/sysv/consts/fileno.h"
 #include "libc/sysv/consts/o.h"
 #include "libc/sysv/consts/ok.h"
