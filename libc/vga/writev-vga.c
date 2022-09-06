d4d24482ce0a7b79a4dc4439024198c323fbb975
tkchia <tkchia-cosmo@gmx.com>
2022-09-06 15:18:25 +0000

    Bare metal VGA: support some VT100 escape sequences & UTF-8
    The code is based on a pared-down version of the pty code
    in tool/build/lib/pty.c.
    
    It currently adds about 12 KiB to the size of the final
    executable (compared to the earlier (simplistic) teletype
    implementation).
    
    Some terminal features, such as kTtyBell & kTtyNocursor,
    are not yet implemented.

--- libc/vga/writev-vga.c
+++ libc/vga/writev-vga.c
@@ -31,105 +31,42 @@
 #include "libc/runtime/pc.internal.h"
 #include "libc/str/str.h"
 
-#define CRTPORT 0x3d4
-#define WIDTH 80
-
-typedef struct {
-  char ch;
-  uint8_t attr;
-} char_cell_t;
-
-typedef char_cell_t char_row_t[WIDTH];
-
-static unsigned short height = 25, curr_row, curr_col;
-static uint8_t curr_attr = 0x07;
-
-static void scroll(void) {
-  unsigned j;
-  uint8_t attr = curr_attr;
-  char_row_t * const vid_buf = (char_row_t *)(BANE + 0xb8000ull);
-  memmove(vid_buf, vid_buf + 1, (height - 1) * sizeof(char_row_t));
-  for (j = 0; j < WIDTH; ++j)
-    vid_buf[height - 1][j] = (char_cell_t){ ' ', attr };
-}
-
-static void may_scroll(void) {
-  if (curr_col >= WIDTH) {
-    curr_col = 0;
-    scroll();
-  }
-}
-
-static void updatexy_vga(void) {
-  unsigned short pos = curr_row * WIDTH + curr_col;
-  outb(CRTPORT, 0x0e);
-  outb(CRTPORT + 1, (unsigned char)(pos >> 8));
-  outb(CRTPORT, 0x0f);
-  outb(CRTPORT + 1, (unsigned char)pos);
-}
-
-static void writec_vga(char c) {
-  /* TODO: handle UTF-8 multi-bytes */
-  /* TODO: handle VT102 escape sequences */
-  /* TODO: maybe make BEL (\a) character code emit an alarm of some sort */
-  char_row_t * const vid_buf = (char_row_t *)(BANE + 0xb8000ull);
-  uint8_t attr = curr_attr;
-  unsigned short col;
-  switch (c) {
-    case '\b':
-      if (curr_col) {
-        col = curr_col - 1;
-        vid_buf[curr_row][col] = (char_cell_t){ ' ', attr };
-        curr_col = col;
-      }
-      break;
-    case '\t':
-      col = curr_col;
-      do {
-        vid_buf[curr_row][col] = (char_cell_t){ ' ', attr };
-        ++col;
-      } while (col % 8 != 0);
-      curr_col = col;
-      may_scroll();
-      break;
-    case '\r':
-      curr_col = 0;
-      break;
-    case '\n':
-      curr_col = 0;
-      if (curr_row < height - 1)
-        ++curr_row;
-      else
-        scroll();
-      break;
-    default:
-      col = curr_col;
-      vid_buf[curr_row][col] = (char_cell_t){ c, attr };
-      curr_col = col + 1;
-      may_scroll();
-  }
-}
+static struct Tty vga_tty;
+#ifdef VGA_USE_WCS
+static wchar_t vga_wcs[VGA_TTY_HEIGHT * VGA_TTY_WIDTH];
+#else
+static wchar_t * const vga_wcs = NULL;
+#endif
 
 ssize_t sys_writev_vga(struct Fd *fd, const struct iovec *iov, int iovlen) {
-  char_row_t * const vid_buf = (char_row_t *)(BANE + 0xb8000ull);
-  size_t i, j, wrote = 0;
+  size_t i, wrote = 0;
+  ssize_t res = 0;
   for (i = 0; i < iovlen; ++i) {
-    const char *input = (const char *)iov[i].iov_base;
-    for (j = 0; j < iov[i].iov_len; ++j) {
-      writec_vga(input[j]);
-      ++wrote;
-    }
+    void *input = iov[i].iov_base;
+    size_t len = iov[i].iov_len;
+    res = _TtyWrite(&vga_tty, input, len);
+    if (res < 0)
+      break;
+    wrote += res;
+    if (res != len)
+      return wrote;
   }
-  updatexy_vga();
+  if (!wrote)
+    return res;
   return wrote;
 }
 
 __attribute__((__constructor__)) static textstartup void _vga_init(void) {
+  void * const vid_buf = (void *)(BANE + 0xb8000ull);
   /* Get the initial cursor position from the BIOS data area. */
   typedef struct {
     unsigned char col, row;
   } bios_curs_pos_t;
   bios_curs_pos_t pos = *(bios_curs_pos_t *)(BANE + 0x0450ull);
-  curr_row = pos.row;
-  curr_col = pos.col;
+  /*
+   * Initialize our tty structure from the current screen contents & current
+   * cursor position.
+   */
+  _StartTty(&vga_tty, VGA_TTY_HEIGHT, VGA_TTY_WIDTH, pos.row, pos.col,
+            vid_buf, vga_wcs);
 }
