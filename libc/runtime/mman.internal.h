540b4e441b8adb9d3e8871b0ba2263514d0c6cd2
tkchia <tkchia-cosmo@gmx.com>
2022-09-16 09:30:46 +0000

    Bare metal: export structure member offsets from C code, rather than compute them by hand

--- libc/runtime/mman.internal.h
+++ libc/runtime/mman.internal.h
@@ -9,14 +9,15 @@ struct mman {
   int32_t pdpi;                          /* 0x0508 */
   int32_t e820n;                         /* 0x050c */
   struct SmapEntry e820[256];            /* 0x0510 */
+  struct SmapEntry e820_end[0];          /* 0x1d10 */
   char pc_drive_base_table[11];          /* 0x1d10 */
   unsigned char pc_drive_type;           /* 0x1d1b */
   unsigned char pc_drive_last_sector;    /* 0x1d1c */
-  unsigned short pc_drive_last_cylinder; /* 0x1d1d */
-  unsigned char pc_drives_attached;      /* 0x1d1f */
-  unsigned char pc_drive_last_head;      /* 0x1d20 */
-  unsigned char pc_drive;                /* 0x1d21 */
-  char bad_idt[6];                       /* 0x1d22 */
+  unsigned short pc_drive_last_cylinder; /* 0x1d1e */
+  unsigned char pc_drives_attached;      /* 0x1d20 */
+  unsigned char pc_drive_last_head;      /* 0x1d21 */
+  unsigned char pc_drive;                /* 0x1d22 */
+  char bad_idt[6];                       /* 0x1d23 */
 };
 
 COSMOPOLITAN_C_END_
