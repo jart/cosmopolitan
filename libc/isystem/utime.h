b7432f46c191672c0d127106b8f96e54606585f6
Gavin Hayes <gavin@computoid.com>
2022-10-04 12:10:01 -0400

    add utime to utime.h

--- libc/isystem/utime.h
+++ libc/isystem/utime.h
@@ -1,4 +1,5 @@
 #ifndef COSMOPOLITAN_LIBC_ISYSTEM_UTIME_H_
 #define COSMOPOLITAN_LIBC_ISYSTEM_UTIME_H_
+#include "libc/time/struct/utimbuf.h"
 #include "libc/time/time.h"
 #endif /* COSMOPOLITAN_LIBC_ISYSTEM_UTIME_H_ */
