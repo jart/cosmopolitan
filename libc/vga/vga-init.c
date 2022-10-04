9926ed74f11222d4bf0c71a1944c2e717a9552b8
tkchia <tkchia-cosmo@gmx.com>
2022-10-04 21:21:33 +0000

    [metal] Refactoring: separate out sys_writev_vga() and _vga_init() modules

--- libc/vga/vga-init.c
+++ libc/vga/vga-init.c
@@ -24,26 +24,47 @@
 │ ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR        │
 │ OTHER DEALINGS IN THE SOFTWARE.                                              │
 ╚─────────────────────────────────────────────────────────────────────────────*/
-#include "libc/calls/struct/fd.internal.h"
-#include "libc/calls/struct/iovec.h"
-#include "libc/calls/struct/iovec.internal.h"
 #include "libc/dce.h"
 #include "libc/vga/vga.internal.h"
+#include "libc/runtime/pc.internal.h"
+#include "libc/str/str.h"
 
-ssize_t sys_writev_vga(struct Fd *fd, const struct iovec *iov, int iovlen) {
-  size_t i, wrote = 0;
-  ssize_t res = 0;
-  for (i = 0; i < iovlen; ++i) {
-    void *output = iov[i].iov_base;
-    size_t len = iov[i].iov_len;
-    res = _TtyWrite(&_vga_tty, output, len);
-    if (res < 0)
-      break;
-    wrote += res;
-    if (res != len)
-      return wrote;
+struct Tty _vga_tty;
+
+__attribute__((__constructor__)) static textstartup void _vga_init(void) {
+  if (IsMetal()) {
+    struct mman *mm = (struct mman *)(BANE + 0x0500);
+    unsigned char vid_type = mm->pc_video_type;
+    unsigned short height = mm->pc_video_height, width = mm->pc_video_width,
+                   stride = mm->pc_video_stride;
+    uint64_t vid_buf_phy = mm->pc_video_framebuffer;
+    void *vid_buf = (void *)(BANE + vid_buf_phy);
+    size_t vid_buf_sz = mm->pc_video_framebuffer_size;
+    /*
+     * Get the initial cursor position from the BIOS data area.  Also get
+     * the height (in scan lines) of each character; this is used to set the
+     * cursor shape.
+     */
+    typedef struct {
+      unsigned char col, row;
+    } bios_curs_pos_t;
+    bios_curs_pos_t pos = *(bios_curs_pos_t *)(BANE + 0x0450ull);
+    uint8_t chr_ht, chr_ht_hi, chr_wid;
+    if (vid_type == PC_VIDEO_TEXT) {
+      chr_ht = *(uint8_t *)(BANE + 0x0485ull),
+      chr_ht_hi = *(uint8_t *)(BANE + 0x0486ull);
+      if (chr_ht_hi != 0 || chr_ht > 32 || chr_ht < 2)
+        chr_ht = VGA_ASSUME_CHAR_HEIGHT_PX;
+    } else
+      chr_ht = VGA_ASSUME_CHAR_HEIGHT_PX;
+    chr_wid = VGA_ASSUME_CHAR_WIDTH_PX;
+    /* Make sure the video buffer is mapped into virtual memory. */
+    __invert_memory_area(mm, __get_pml4t(), vid_buf_phy, vid_buf_sz, PAGE_RW);
+    /*
+     * Initialize our tty structure from the current screen geometry,
+     * screen contents, cursor position, & character dimensions.
+     */
+    _StartTty(&_vga_tty, vid_type, height, width, stride, pos.row, pos.col,
+              chr_ht, chr_wid, vid_buf, false);
   }
-  if (!wrote)
-    return res;
-  return wrote;
 }
