5ed84e04f8872cd1340a61b702084d1b6530d75b
ahgamut <ahgamut@gmail.com>
2022-11-06 20:44:37 +0530

    remove libc/sock/if.h
    libc/sock/if.h is removed because:
    
    - it contained only IFNAMSIZ, which was also defined in
      libc/sock/struct/ifreq.h
    - it was not included in any file EXCEPT libc/isystem/net/if.h, which
      also included the libc/sock/struct/ifreq.h
    
    hence we remove this file, and move the IFNAMSIZ definition to
    libc/sock/struct/ifreq.h which is included in isystem.

--- libc/isystem/net/if.h
+++ libc/isystem/net/if.h
@@ -1,6 +1,5 @@
 #ifndef COSMOPOLITAN_LIBC_ISYSTEM_NET_IF_H_
 #define COSMOPOLITAN_LIBC_ISYSTEM_NET_IF_H_
-#include "libc/sock/if.h"
 #include "libc/sock/struct/ifconf.h"
 #include "libc/sock/struct/ifreq.h"
 #include "libc/sysv/consts/iff.h"
