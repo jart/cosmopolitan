65b5e2e5596b98fe9522baa0ef75c1ff99b8c6d8
Gabriel Ravier <gabravier@gmail.com>
2023-03-31 22:43:56 +0200

    Fix printf precision/field width being limited by internal buffer size
    The C standard, when defining field width and precision, never gives
    any limit on the values used for them (except, I believe, that they
    fit within an int). In other words, if the user gives a field width of
    32145 and a precision of 9218, the implementation has to handle these
    values correctly. However, when such kinds of high numbers are used
    with integer conversions, cosmopolitan is limited by an internal
    buffer size of 144, which means precisions and field widths have to
    fit within this, which violates the standard.
    
    This means that for example, the following program:
    
     #include <stdio.h>
     #include <string.h>
    
    int main()
    {
        char buf2[512] = {};
    
        int i = snprintf(buf2, sizeof(buf2), "%.9999u", 10);
        printf("%d %zu\n", i, strlen(buf2));
    }
    
    would, instead of printing "9999 511" (the correct output), instead
    print "144 144" under cosmopolitan.
    
    This patch fixes this.

---
+++ test/libc/fmt/snprintf_test.c
@@ -0,0 +1,29 @@
+/*-*- mode:c;indent-tabs-mode:nil;c-basic-offset:2;tab-width:8;coding:utf-8 -*-│
+│vi: set net ft=c ts=2 sts=2 sw=2 fenc=utf-8                                :vi│
+╞══════════════════════════════════════════════════════════════════════════════╡
+│ Copyright 2023 Gabriel Ravier                                                │
+│                                                                              │
+│ Permission to use, copy, modify, and/or distribute this software for         │
+│ any purpose with or without fee is hereby granted, provided that the         │
+│ above copyright notice and this permission notice appear in all copies.      │
+│                                                                              │
+│ THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL                │
+│ WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED                │
+│ WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE             │
+│ AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL         │
+│ DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR        │
+│ PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER               │
+│ TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR             │
+│ PERFORMANCE OF THIS SOFTWARE.                                                │
+╚─────────────────────────────────────────────────────────────────────────────*/
+#include "libc/fmt/fmt.h"
+#include "libc/str/str.h"
+#include "libc/testlib/testlib.h"
+
+TEST(snprintf, testVeryLargePrecision) {
+  char buf[512] = {};
+  int i = snprintf(buf, sizeof(buf), "%.9999u", 10);
+
+  ASSERT_EQ(i, 9999);
+  ASSERT_EQ(strlen(buf), 511);
+}
