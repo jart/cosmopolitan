jart/cosmopolitan

commit 4778cd4d27131374053ea797e14021fea7bb3d55

Fix bugs in termios library and cleanup code
This change fixes an issue with the tcflow() magic numbers that was
causing bash to freeze up on Linux. While auditing termios polyfills,
several other issues were identified with XNU/BSD compatibility.

Out of an abundance of caution this change undefines as much surface
area from libc/calls/struct/termios.h as possible, so that autoconf
scripts are less likely to detect non-POSIX teletypewriter APIs that
haven't been polyfilled by Cosmopolitan.

This is a *breaking change* for your static archives in /opt/cosmos if
you use the cosmocc toolchain. That's because this change disables the
ioctl() undiamonding trick for code outside the monorepo, specifically
because it'll lead to brittle ABI breakages like this. If you're using
the cosmocc toolchain, you'll need to rebuild libraries like ncurses,
readline, etc. Yes diamonds cause bloat. To work around that, consider
using tcgetwinsize() instead of ioctl(TIOCGWINSZ) since it'll help you
avoid pulling every single ioctl-related polyfill into the linkage.

The cosmocc script was specifying -DNDEBUG for some reason. It's fixed.

libc/calls/tcflow.c
@@ -17,6 +17,7 @@
 │ PERFORMANCE OF THIS SOFTWARE.                                                │
 ╚─────────────────────────────────────────────────────────────────────────────*/
 #include "libc/calls/internal.h"
+#include "libc/calls/struct/metatermios.internal.h"
 #include "libc/calls/struct/termios.h"
 #include "libc/calls/syscall-sysv.internal.h"
 #include "libc/calls/syscall_support-nt.internal.h"
@@ -32,6 +33,13 @@
 #define kNtPurgeTxabort 1
 #define kNtPurgeRxabort 2
 
+#define TCXONC    0x0000540A  // linux
+#define TIOCGETA  0x40487413  // bsd
+#define TIOCSTOP  0x2000746f  // bsd
+#define TIOCSTART 0x2000746e  // bsd
+#define TIOCIXON  0x20007481  // xnu
+#define TIOCIXOFF 0x20007480  // xnu
+
 static const char *DescribeFlow(char buf[12], int action) {
   if (action == TCOOFF) return "TCOOFF";
   if (action == TCOON) return "TCOON";
@@ -41,18 +49,41 @@ static const char *DescribeFlow(char buf[12], int action) {
   return buf;
 }
 
-static int sys_tcflow_bsd(int fd, int action) {
-  int rc;
-  uint8_t c;
-  struct termios t;
-  if (action == TCOOFF) return sys_ioctl(fd, TIOCSTOP, 0);
-  if (action == TCOON) return sys_ioctl(fd, TIOCSTART, 0);
-  if (action != TCIOFF && action != TCION) return einval();
-  if (sys_ioctl(fd, TCGETS, &t) == -1) return -1;
-  c = t.c_cc[action == TCIOFF ? VSTOP : VSTART];
-  if (c == 255) return 0;  // code is disabled
-  if (sys_write(fd, &c, 1) == -1) return -1;
+static int sys_tcflow_bsd_write(int fd, int cc) {
+  unsigned char c;
+  struct termios_bsd term;
+  if (sys_ioctl(fd, TIOCGETA, &term) == -1) {
+    return -1;
+  }
+  if ((c = term.c_cc[cc]) != _POSIX_VDISABLE &&
+      sys_write(fd, &c, sizeof(c)) == -1) {
+    return -1;
+  }
   return 0;
+}
+
+static int sys_tcflow_bsd(int fd, int action) {
+  switch (action) {
+    case TCOOFF:
+      return sys_ioctl(fd, TIOCSTOP, 0);
+    case TCOON:
+      return sys_ioctl(fd, TIOCSTART, 0);
+    case TCION:
+      if (IsXnu()) {
+        return sys_ioctl(fd, TIOCIXON, 0);
+      } else {
+        return sys_tcflow_bsd_write(fd, VSTART);
+      }
+    case TCIOFF:
+      if (IsXnu()) {
+        return sys_ioctl(fd, TIOCIXOFF, 0);
+      } else {
+        return sys_tcflow_bsd_write(fd, VSTOP);
+      }
+      return 0;
+    default:
+      return einval();
+  }
 }
 
 static dontinline textwindows int sys_tcflow_nt(int fd, int action) {
@@ -60,14 +91,19 @@ static dontinline textwindows int sys_tcflow_nt(int fd, int action) {
   int64_t h;
   if (!__isfdopen(fd)) return ebadf();
   h = g_fds.p[fd].handle;
-  if (action == TCOOFF) {
-    ok = PurgeComm(h, kNtPurgeTxabort);
-  } else if (action == TCIOFF) {
-    ok = PurgeComm(h, kNtPurgeRxabort);
-  } else if (action == TCOON || action == TCION) {
-    ok = ClearCommBreak(h);
-  } else {
-    return einval();
+  switch (action) {
+    case TCOON:
+    case TCION:
+      ok = ClearCommBreak(h);
+      break;
+    case TCOOFF:
+      ok = PurgeComm(h, kNtPurgeTxabort);
+      break;
+    case TCIOFF:
+      ok = PurgeComm(h, kNtPurgeRxabort);
+      break;
+    default:
+      return einval();
   }
   return ok ? 0 : __winerr();
 }
@@ -92,14 +128,14 @@ static dontinline textwindows int sys_tcflow_nt(int fd, int action) {
  */
 int tcflow(int fd, int action) {
   int rc;
-  if (IsMetal()) {
-    rc = enosys();
+  if (IsLinux()) {
+    rc = sys_ioctl(fd, TCXONC, action);
   } else if (IsBsd()) {
-    rc = sys_ioctl(fd, TCXONC, action);
-  } else if (!IsWindows()) {
-    rc = sys_ioctl(fd, TCXONC, action);
-  } else {
+    rc = sys_tcflow_bsd(fd, action);
+  } else if (IsWindows()) {
     rc = sys_tcflow_nt(fd, action);
+  } else {
+    rc = enosys();
   }
   STRACE("tcflow(%d, %s) → %d% m", fd, DescribeFlow(alloca(12), action), rc);
   return rc;

libc/calls/tcgetattr.c
@@ -16,21 +16,56 @@
 │ TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR             │
 │ PERFORMANCE OF THIS SOFTWARE.                                                │
 ╚─────────────────────────────────────────────────────────────────────────────*/
-#include "libc/calls/ioctl.h"
-#include "libc/calls/termios.h"
-#include "libc/sysv/consts/termios.h"
+#include "libc/calls/internal.h"
+#include "libc/calls/struct/metatermios.internal.h"
+#include "libc/calls/struct/termios.h"
+#include "libc/calls/syscall-sysv.internal.h"
+#include "libc/calls/termios.internal.h"
+#include "libc/calls/ttydefaults.h"
+#include "libc/dce.h"
+#include "libc/intrin/asan.internal.h"
+#include "libc/intrin/strace.internal.h"
+#include "libc/str/str.h"
+#include "libc/sysv/errfuns.h"
+
+#define TCGETS   0x00005401  // linux
+#define TIOCGETA 0x40487413  // bsd
+
+int tcgetattr_nt(int, struct termios *);
+
+static int tcgetattr_metal(int fd, struct termios *tio) {
+  bzero(tio, sizeof(*tio));
+  tio->c_iflag = TTYDEF_IFLAG;
+  tio->c_oflag = TTYDEF_OFLAG;
+  tio->c_lflag = TTYDEF_LFLAG;
+  tio->c_cflag = TTYDEF_CFLAG;
+  return 0;
+}
+
+static int tcgetattr_bsd(int fd, struct termios *tio) {
+  int rc;
+  union metatermios mt;
+  if ((rc = sys_ioctl(fd, TIOCGETA, &mt)) != -1) {
+    if (IsXnu()) {
+      COPY_TERMIOS(tio, &mt.xnu);
+    } else {
+      COPY_TERMIOS(tio, &mt.bsd);
+    }
+  }
+  return rc;
+}
 
 /**
  * Obtains the termios struct.
  *
- * Here are the general defaults you can expect across platforms:
+ * Here are the approximate defaults you can expect across platforms:
  *
  *     c_iflag        = ICRNL IXON
  *     c_oflag        = OPOST ONLCR NL0 CR0 TAB0 BS0 VT0 FF0
  *     c_cflag        = ISIG CREAD CS8
  *     c_lflag        = ISIG ICANON ECHO ECHOE ECHOK IEXTEN ECHOCTL ECHOKE
- *     c_ispeed       = 38400
- *     c_ospeed       = 38400
+ *     cfgetispeed()  = B38400
+ *     cfgetospeed()  = B38400
  *     c_cc[VINTR]    = CTRL-C
  *     c_cc[VQUIT]    = CTRL-\   # ignore this comment
  *     c_cc[VERASE]   = CTRL-?
@@ -54,6 +89,25 @@
  * @return 0 on success, or -1 w/ errno
  * @asyncsignalsafe
  */
-int(tcgetattr)(int fd, struct termios *tio) {
-  return ioctl(fd, TCGETS, tio);
+int tcgetattr(int fd, struct termios *tio) {
+  int rc;
+  if (fd < 0) {
+    rc = einval();
+  } else if (!tio || (IsAsan() && !__asan_is_valid(tio, sizeof(*tio)))) {
+    rc = efault();
+  } else if (fd < g_fds.n && g_fds.p[fd].kind == kFdZip) {
+    rc = enotty();
+  } else if (IsLinux()) {
+    rc = sys_ioctl(fd, TCGETS, tio);
+  } else if (IsBsd()) {
+    rc = tcgetattr_bsd(fd, tio);
+  } else if (IsMetal()) {
+    rc = tcgetattr_metal(fd, tio);
+  } else if (IsWindows()) {
+    rc = tcgetattr_nt(fd, tio);
+  } else {
+    rc = enosys();
+  }
+  STRACE("tcgetattr(%d, %p) → %d% m", fd, tio, rc);
+  return rc;
 }

libc/calls/tcsetattr.c
@@ -16,10 +16,76 @@
 │ TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR             │
 │ PERFORMANCE OF THIS SOFTWARE.                                                │
 ╚─────────────────────────────────────────────────────────────────────────────*/
+#include "libc/calls/internal.h"
 #include "libc/calls/ioctl.h"
+#include "libc/calls/struct/metatermios.internal.h"
+#include "libc/calls/syscall-sysv.internal.h"
 #include "libc/calls/termios.h"
+#include "libc/calls/termios.internal.h"
+#include "libc/dce.h"
+#include "libc/fmt/itoa.h"
+#include "libc/intrin/asan.internal.h"
+#include "libc/intrin/strace.internal.h"
+#include "libc/intrin/weaken.h"
+#include "libc/mem/alloca.h"
 #include "libc/sysv/consts/termios.h"
 #include "libc/sysv/errfuns.h"
+
+#define TCSETS    0x5402
+#define TCSETSW   0x5403
+#define TCSETSF   0x5404
+#define TIOCSETA  0x80487414
+#define TIOCSETAW 0x80487415
+#define TIOCSETAF 0x80487416
+
+void __on_tcsetattr(int);
+int tcsetattr_nt(int, int, const struct termios *);
+
+static const char *DescribeTcsa(char buf[12], int opt) {
+  if (opt == TCSANOW) return "TCSANOW";
+  if (opt == TCSADRAIN) return "TCSADRAIN";
+  if (opt == TCSAFLUSH) return "TCSAFLUSH";
+  FormatInt32(buf, opt);
+  return buf;
+}
+
+static int tcsetattr_impl(int fd, int opt, const struct termios *tio) {
+  if (fd < 0) {
+    return einval();
+  }
+
+  if (fd < g_fds.n && g_fds.p[fd].kind == kFdZip) {
+    return enotty();
+  }
+
+  if (0 <= fd && fd <= 2 && _weaken(__on_tcsetattr)) {
+    static bool once;
+    if (!once) {
+      _weaken(__on_tcsetattr)(fd);
+      once = true;
+    }
+  }
+
+  if (IsAsan() && !__asan_is_valid(tio, sizeof(*tio))) {
+    return efault();
+  }
+
+  if (IsMetal()) {
+    return 0;
+  }
+
+  if (IsWindows()) {
+    return tcsetattr_nt(fd, opt, tio);
+  }
+
+  if (IsLinux() || IsBsd()) {
+    union metatermios mt;
+    return sys_ioctl(fd, (IsLinux() ? TCSETS : TIOCSETA) + opt,
+                     __termios2host(&mt, tio));
+  }
+
+  return enosys();
+}
 
 /**
  * Sets struct on teletypewriter w/ drains and flushes.
@@ -32,15 +98,10 @@
  * @return 0 on success, -1 w/ errno
  * @asyncsignalsafe
  */
-int(tcsetattr)(int fd, int opt, const struct termios *tio) {
-  switch (opt) {
-    case TCSANOW:
-      return ioctl(fd, TCSETS, (void *)tio);
-    case TCSADRAIN:
-      return ioctl(fd, TCSETSW, (void *)tio);
-    case TCSAFLUSH:
-      return ioctl(fd, TCSETSF, (void *)tio);
-    default:
-      return einval();
-  }
+int tcsetattr(int fd, int opt, const struct termios *tio) {
+  int rc;
+  rc = tcsetattr_impl(fd, opt, tio);
+  STRACE("tcsetattr(%d, %s, %p) → %d% m", fd, DescribeTcsa(alloca(12), opt),
+         tio, rc);
+  return rc;
 }

libc/sysv/consts/TCGETS.S
@@ -1,2 +1,0 @@
-#include "libc/sysv/consts/syscon.internal.h"
-.syscon termios,TCGETS,0x5401,0x5401,0x40487413,0x40487413,0x402c7413,0x402c7413,0x402c7413,0

libc/sysv/consts/TCSETS.S
@@ -1,2 +1,0 @@
-#include "libc/sysv/consts/syscon.internal.h"
-.syscon termios,TCSETS,0x5402,0x5402,0x80487414,0x80487414,0x802c7414,0x802c7414,0x802c7414,0x5402

libc/calls/struct/termios.h
@@ -1,19 +1,20 @@
 #ifndef COSMOPOLITAN_LIBC_CALLS_STRUCT_TERMIOS_H_
 #define COSMOPOLITAN_LIBC_CALLS_STRUCT_TERMIOS_H_
+
+#define NCCS 20
+
 #if !(__ASSEMBLER__ + __LINKER__ + 0)
 COSMOPOLITAN_C_START_
 
-struct termios {     /* GNU/Systemd ABI */
-  uint32_t c_iflag;  /* input modes */
-  uint32_t c_oflag;  /* output modes */
-  uint32_t c_cflag;  /* control modes */
-  uint32_t c_lflag;  /* local modes */
-  uint8_t c_cc[20];  /* code mappings */
-  uint32_t c_ispeed; /* input speed */
-  uint32_t c_ospeed; /* output speed */
+struct termios {      /* cosmo abi */
+  uint32_t c_iflag;   /* input modes */
+  uint32_t c_oflag;   /* output modes */
+  uint32_t c_cflag;   /* control modes */
+  uint32_t c_lflag;   /* local modes */
+  uint8_t c_cc[NCCS]; /* code mappings */
+  uint32_t _c_ispeed; /* use cfgetispeed() and cfsetispeed() */
+  uint32_t _c_ospeed; /* use cfgetospeed() and cfsetospeed() */
 };
-
-#define c_line c_cc[0] /* line discipline */
 
 COSMOPOLITAN_C_END_
 #endif /* !(__ASSEMBLER__ + __LINKER__ + 0) */

commit 053681cb97aa3204e4577b4f2773bd10f537cf9e

Fix BSD regressions
The recent change to crt.S that aggressively aligns the system-provided
stack has been rolled back on non-Linux until we can find a better way,
since it can cause a segfault early in execution on several platforms.

This change fixes a regression in tcgetattr() and tcsetattr() on OpenBSD
and NetBSD caused by 4778cd4d27131374053ea797e14021fea7bb3d55.

This change has been tested across the runitd test fleet which is green.

libc/calls/tcflow.c
@@ -34,7 +34,6 @@
 #define kNtPurgeRxabort 2
 
 #define TCXONC    0x0000540A  // linux
-#define TIOCGETA  0x40487413  // bsd
 #define TIOCSTOP  0x2000746f  // bsd
 #define TIOCSTART 0x2000746e  // bsd
 #define TIOCIXON  0x20007481  // xnu
@@ -52,7 +51,7 @@ static const char *DescribeFlow(char buf[12], int action) {
 static int sys_tcflow_bsd_write(int fd, int cc) {
   unsigned char c;
   struct termios_bsd term;
-  if (sys_ioctl(fd, TIOCGETA, &term) == -1) {
+  if (sys_ioctl(fd, TCGETS, &term) == -1) {
     return -1;
   }
   if ((c = term.c_cc[cc]) != _POSIX_VDISABLE &&

libc/calls/tcgetattr.c
@@ -28,9 +28,6 @@
 #include "libc/str/str.h"
 #include "libc/sysv/errfuns.h"
 
-#define TCGETS   0x00005401  // linux
-#define TIOCGETA 0x40487413  // bsd
-
 int tcgetattr_nt(int, struct termios *);
 
 static int tcgetattr_metal(int fd, struct termios *tio) {
@@ -45,7 +42,7 @@ static int tcgetattr_metal(int fd, struct termios *tio) {
 static int tcgetattr_bsd(int fd, struct termios *tio) {
   int rc;
   union metatermios mt;
-  if ((rc = sys_ioctl(fd, TIOCGETA, &mt)) != -1) {
+  if ((rc = sys_ioctl(fd, TCGETS, &mt)) != -1) {
     if (IsXnu()) {
       COPY_TERMIOS(tio, &mt.xnu);
     } else {

libc/calls/tcsetattr.c
@@ -31,13 +31,6 @@
 #include "libc/sysv/consts/termios.h"
 #include "libc/sysv/errfuns.h"
 
-#define TCSETS    0x5402
-#define TCSETSW   0x5403
-#define TCSETSF   0x5404
-#define TIOCSETA  0x80487414
-#define TIOCSETAW 0x80487415
-#define TIOCSETAF 0x80487416
-
 void __on_tcsetattr(int);
 int tcsetattr_nt(int, int, const struct termios *);
 
@@ -80,8 +73,7 @@ static int tcsetattr_impl(int fd, int opt, const struct termios *tio) {
 
   if (IsLinux() || IsBsd()) {
     union metatermios mt;
-    return sys_ioctl(fd, (IsLinux() ? TCSETS : TIOCSETA) + opt,
-                     __termios2host(&mt, tio));
+    return sys_ioctl(fd, TCSETS + opt, __termios2host(&mt, tio));
   }
 
   return enosys();

libc/crt/crt.S
@@ -72,9 +72,9 @@ _start:
 //	setup a stack frame
 //	align stack to GetStackSize() so GetStackAddr() is fast
 	.weak	ape_stack_memsz
-	mov	$ape_stack_memsz,%r8d
-	mov	$ape_stack_align,%r9d
-	cmp	$_HOSTMETAL,%cl
+	mov	$ape_stack_memsz,%r9d
+	mov	$ape_stack_align,%r8d
+	cmp	$_HOSTLINUX,%cl
 	cmove	%r9d,%r8d
 	neg	%r8
 	and	%r8,%rsp

libc/sysv/consts.sh
@@ -1318,6 +1318,8 @@ syscon	sched	SCHED_RESET_ON_FORK			0x40000000		0x40000000		0			0			0			0			0			0
 #		= TIOCSETA → About  3,110 results (0.41 seconds)
 #
 #	group	name					GNU/Systemd		GNU/Systemd (Aarch64)	XNU's Not UNIX!		MacOS (Arm64)		FreeBSD			OpenBSD			NetBSD			The New Technology	Commentary
+syscon	termios	TCGETS					0x5401			0x5401			0x40487413		0x40487413		0x402c7413		0x402c7413		0x402c7413		0x5401			# Gets console settings; tcgetattr(tty, argp) → ioctl(tty, TCGETS, struct termios *argp); polyfilled NT
+syscon	termios	TCSETS					0x5402			0x5402			0x80487414		0x80487414		0x802c7414		0x802c7414		0x802c7414		0x5402			# Sets console settings; = ioctl(tty, TCSETS, const struct termios *argp); polyfilled NT
 syscon	termios	TIOCGWINSZ				0x5413			0x5413			1074295912		1074295912		1074295912		1074295912		1074295912		0x5413			# ioctl(tty, TIOCGWINSZ, struct winsize *argp); polyfilled NT
 syscon	termios	TIOCSWINSZ				0x5414			0x5414			0x80087467		0x80087467		0x80087467		0x80087467		0x80087467		0x5414			# ioctl(tty, TIOCSWINSZ, const struct winsize *argp) (faked NT)
 syscon	termios	TIOCINQ					0x541b			0x541b			0x4004667f		0x4004667f		0x4004667f		0x4004667f		0x4004667f		0x4004667f		# [Linuxism] same as FIONREAD
@@ -1441,6 +1443,9 @@ syscon	termios	VLNEXT					15+1			15+1			14			14			14			14			14			15			# termios.
 syscon	termios	VEOL2					16+1			16+1			2			2			2			2			2			16			# termios.c_cc[VEOL2]=𝑥
 syscon	termios	_POSIX_VDISABLE				0			0			255			255			255			255			255			0			# termios.c_cc tombstone value
 
+#	tcflush() magic numbers
+#
+#	group	name					GNU/Systemd		GNU/Systemd (Aarch64)	XNU's Not UNIX!		MacOS (Arm64)		FreeBSD			OpenBSD			NetBSD			The New Technology	Commentary
 syscon	termios	TCIFLUSH				0			0			1			1			1			1			1			0			# see tcflush; FREAD on BSD; faked nt
 syscon	termios	TCOFLUSH				1			1			2			2			2			2			2			1			# see tcflush; FWRITE on BSD; faked nt
 syscon	termios	TCIOFLUSH				2			2			3			3			3			3			3			2			# see tcflush; FREAD|FWRITE on BSD; faked nt

libc/sysv/consts/TCGETS.S
@@ -1,0 +1,2 @@
+#include "libc/sysv/consts/syscon.internal.h"
+.syscon termios,TCGETS,0x5401,0x5401,0x40487413,0x40487413,0x402c7413,0x402c7413,0x402c7413,0x5401

libc/sysv/consts/TCSETS.S
@@ -1,0 +1,2 @@
+#include "libc/sysv/consts/syscon.internal.h"
+.syscon termios,TCSETS,0x5402,0x5402,0x80487414,0x80487414,0x802c7414,0x802c7414,0x802c7414,0x5402

libc/sysv/consts/termios.h
@@ -73,12 +73,6 @@ extern const uint32_t TAB1;
 extern const uint32_t TAB2;
 extern const uint32_t TAB3;
 extern const uint32_t TABDLY;
-extern const int TCIFLUSH;
-extern const int TCIOFLUSH;
-extern const int TCOFLUSH;
-extern const int TCSADRAIN;
-extern const int TCSAFLUSH;
-extern const int TCSANOW;
 extern const uint64_t TIOCCONS;
 extern const uint64_t TIOCGETD;
 extern const uint64_t TIOCGWINSZ;
@@ -115,11 +109,6 @@ extern const uint8_t VWERASE;
 extern const uint32_t XCASE;
 extern const uint32_t XTABS;
 extern const uint32_t CRTSCTS;
-
-#define TCOOFF 0
-#define TCOON  1
-#define TCIOFF 2
-#define TCION  3
 
 #define BRKINT  0x02
 #define ICRNL   0x0100
@@ -202,12 +191,6 @@ extern const uint32_t CRTSCTS;
 #define PARENB     PARENB
 #define PARODD     PARODD
 #define PENDIN     PENDIN
-#define TCIFLUSH   TCIFLUSH
-#define TCIOFLUSH  TCIOFLUSH
-#define TCOFLUSH   TCOFLUSH
-#define TCSADRAIN  1
-#define TCSAFLUSH  2
-#define TCSANOW    0
 #define TIOCCONS   TIOCCONS
 #define TIOCGETD   TIOCGETD
 #define TIOCGWINSZ TIOCGWINSZ
@@ -240,8 +223,26 @@ extern const uint32_t CRTSCTS;
 #define VWERASE    VWERASE
 #define XCASE      XCASE
 
+/* tcsetattr() */
+#define TCSANOW   0
+#define TCSAFLUSH 2
+#define TCSADRAIN 1
+extern const unsigned long TCGETS; /* use tcgetattr() */
+extern const unsigned long TCSETS; /* use tcsetattr() */
 
+/* tcflush() */
+extern const int TCIFLUSH;
+extern const int TCOFLUSH;
+extern const int TCIOFLUSH;
+#define TCIFLUSH  TCIFLUSH
+#define TCOFLUSH  TCOFLUSH
+#define TCIOFLUSH TCIOFLUSH
 
+/* tcflow() */
+#define TCOOFF 0
+#define TCOON  1
+#define TCIOFF 2
+#define TCION  3
 
 COSMOPOLITAN_C_END_
 #endif /* !(__ASSEMBLER__ + __LINKER__ + 0) */

test/libc/calls/printargs_test.c
@@ -17,8 +17,7 @@
 │ PERFORMANCE OF THIS SOFTWARE.                                                │
 ╚─────────────────────────────────────────────────────────────────────────────*/
 #include "libc/calls/calls.h"
-#include "libc/runtime/internal.h"
-#include "libc/sysv/consts/o.h"
+#include "libc/runtime/runtime.h"
 #include "libc/testlib/testlib.h"
 #include "libc/x/x.h"
 