jart/cosmopolitan

commit ec957491ead205cb36470a86adb75ae2b8464015

Make posix_spawn faster on Windows

libc/calls/winstdin1.c
@@ -135,8 +135,8 @@ dontasan dontubsan dontinstrument textwindows void WinMainStdin(void) {
     NTTRACE("<stdin> CreateThread failed");
     return;
   }
-  __imp_CloseHandle(hThread);
-  g_fds.stdin.handle = hStdin;
+  g_fds.stdin.thread = hStdin;
+  g_fds.stdin.handle = hThread;
   g_fds.stdin.reader = hReader;
   g_fds.stdin.writer = hWriter;
   g_fds.stdin.inisem = hSemaphore;

libc/calls/calls.mk
@@ -205,6 +205,8 @@ o/$(MODE)/libc/calls/swapcontext.o: libc/calls/swapcontext.S
 	@$(COMPILE) -AOBJECTIFY.S $(OBJECTIFY.S) $(OUTPUT_OPTION) -c $<
 o/$(MODE)/libc/calls/tailcontext.o: libc/calls/tailcontext.S
 	@$(COMPILE) -AOBJECTIFY.S $(OBJECTIFY.S) $(OUTPUT_OPTION) -c $<
+o/$(MODE)/libc/calls/switchstacks.o: libc/calls/switchstacks.S
+	@$(COMPILE) -AOBJECTIFY.S $(OBJECTIFY.S) $(OUTPUT_OPTION) -c $<
 
 LIBC_CALLS_LIBS = $(foreach x,$(LIBC_CALLS_ARTIFACTS),$($(x)))
 LIBC_CALLS_SRCS = $(foreach x,$(LIBC_CALLS_ARTIFACTS),$($(x)_SRCS))

libc/calls/copy_file_range.c
@@ -16,6 +16,7 @@
 │ TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR             │
 │ PERFORMANCE OF THIS SOFTWARE.                                                │
 ╚─────────────────────────────────────────────────────────────────────────────*/
+#include "libc/atomic.h"
 #include "libc/calls/blockcancel.internal.h"
 #include "libc/calls/calls.h"
 #include "libc/calls/cp.internal.h"
@@ -33,7 +34,7 @@
 #include "libc/sysv/errfuns.h"
 
 static struct CopyFileRange {
-  _Atomic(uint32_t) once;
+  atomic_uint once;
   bool ok;
 } g_copy_file_range;
 

libc/calls/raise.c
@@ -21,6 +21,8 @@
 #include "libc/calls/syscall-sysv.internal.h"
 #include "libc/dce.h"
 #include "libc/intrin/strace.internal.h"
+#include "libc/intrin/weaken.h"
+#include "libc/nt/runtime.h"
 #include "libc/runtime/internal.h"
 #include "libc/sysv/consts/sicode.h"
 #include "libc/sysv/consts/sig.h"
@@ -62,10 +64,20 @@ int raise(int sig) {
     RaiseSigFpe();
     rc = 0;
 #endif
-  } else if (!IsWindows() && !IsMetal()) {
+  } else if (IsLinux() || IsXnu() || IsFreebsd() || IsOpenbsd() || IsNetbsd()) {
     rc = sys_tkill(gettid(), sig, 0);
+  } else if (IsWindows() || IsMetal()) {
+    if (IsWindows() && sig == SIGKILL) {
+      // TODO(jart): Isn't this implemented by __sig_raise()?
+      if (_weaken(__restore_console_win32)) {
+        _weaken(__restore_console_win32)();
+      }
+      ExitProcess(sig);
+    } else {
+      rc = __sig_raise(sig, SI_TKILL);
+    }
   } else {
-    rc = __sig_raise(sig, SI_TKILL);
+    __builtin_unreachable();
   }
   STRACE("...raise(%G) → %d% m", sig, rc);
   return rc;

libc/calls/struct/fd.internal.h
@@ -41,6 +41,7 @@ struct StdinRelay {
   int64_t handle; /* should == g_fds.p[0].handle */
   int64_t reader; /* ReadFile() use this instead */
   int64_t writer; /* only used by WinStdinThread */
+  int64_t thread; /* handle for the stdio thread */
 };
 
 struct Fds {

libc/calls/switchstacks.S
@@ -1,7 +1,7 @@
-/*-*- mode:c;indent-tabs-mode:nil;c-basic-offset:2;tab-width:8;coding:utf-8 -*-│
-│vi: set net ft=c ts=2 sts=2 sw=2 fenc=utf-8                                :vi│
+/*-*- mode:unix-assembly; indent-tabs-mode:t; tab-width:8; coding:utf-8     -*-│
+│vi: set et ft=asm ts=8 tw=8 fenc=utf-8                                     :vi│
 ╞══════════════════════════════════════════════════════════════════════════════╡
-│ Copyright 2022 Justine Alexandra Roberts Tunney                              │
+│ Copyright 2023 Justine Alexandra Roberts Tunney                              │
 │                                                                              │
 │ Permission to use, copy, modify, and/or distribute this software for         │
 │ any purpose with or without fee is hereby granted, provided that the         │
@@ -16,38 +16,19 @@
 │ TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR             │
 │ PERFORMANCE OF THIS SOFTWARE.                                                │
 ╚─────────────────────────────────────────────────────────────────────────────*/
-#include "libc/calls/calls.h"
-#include "libc/calls/sig.internal.h"
-#include "libc/calls/syscall-sysv.internal.h"
-#include "libc/calls/syscall_support-sysv.internal.h"
-#include "libc/dce.h"
-#include "libc/intrin/strace.internal.h"
-#include "libc/sysv/consts/sicode.h"
+#include "libc/macros.internal.h"
 
-// OpenBSD has an optional `tib` parameter for extra safety
-int __tkill(int tid, int sig, void *tib) {
-  int rc;
-  if (!IsWindows() && !IsMetal()) {
-    rc = sys_tkill(tid, sig, tib);
-  } else {
-    rc = __sig_add(tid, sig, SI_TKILL);
-  }
-  STRACE("tkill(%d, %G) → %d% m", tid, sig, rc);
-  return rc;
-}
-
-/**
- * Kills thread.
- *
- * @param tid is thread id
- * @param sig does nothing on xnu
- * @return 0 on success, or -1 w/ errno
- * @raise EAGAIN if `RLIMIT_SIGPENDING` was exceeded
- * @raise EINVAL if `tid` or `sig` were invalid
- * @raise ESRCH if no such `tid` existed
- * @raise EPERM if permission was denied
- * @asyncsignalsafe
- */
-int tkill(int tid, int sig) {
-  return __tkill(tid, sig, 0);
-}
+__switch_stacks:
+#ifdef __x86_64__
+	mov	%r9,%rsp
+	and	$-16,%rsp
+	xor	%rbp,%rbp
+	call	*%r8
+#elif defined(__aarch64__)
+	and	sp,x5,#-16
+	mov	x29,0
+	blr	x4
+#else
+#error "unsupported architecture"
+#endif
+	.endfn	__switch_stacks,globl

libc/intrin/cosmo_once.c
@@ -16,6 +16,7 @@
 │ TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR             │
 │ PERFORMANCE OF THIS SOFTWARE.                                                │
 ╚─────────────────────────────────────────────────────────────────────────────*/
+#include "libc/atomic.h"
 #include "libc/cosmo.h"
 #include "libc/errno.h"
 #include "libc/intrin/atomic.h"
@@ -35,7 +36,7 @@
  *
  * @return 0 on success, or errno on error
  */
-errno_t cosmo_once(_Atomic(uint32_t) *once, void init(void)) {
+errno_t cosmo_once(atomic_uint *once, void init(void)) {
   uint32_t old;
   switch ((old = atomic_load_explicit(once, memory_order_relaxed))) {
     case INIT:

libc/intrin/createdirectory.c
@@ -30,13 +30,12 @@ __msabi extern typeof(CreateDirectory) *const __imp_CreateDirectoryW;
  * @return handle, or -1 on failure
  * @note this wrapper takes care of ABI, STRACE(), and __winerr()
  */
-textwindows bool32
-CreateDirectory(const char16_t *lpPathName,
-                struct NtSecurityAttributes *lpSecurityAttributes) {
+textwindows bool32 CreateDirectory(const char16_t *lpPathName,
+                                   struct NtSecurityAttributes *lpSecurity) {
   bool32 ok;
-  ok = __imp_CreateDirectoryW(lpPathName, lpSecurityAttributes);
+  ok = __imp_CreateDirectoryW(lpPathName, lpSecurity);
   if (!ok) __winerr();
   NTTRACE("CreateDirectory(%#hs, %s) → %hhhd% m", lpPathName,
-          DescribeNtSecurityAttributes(lpSecurityAttributes), ok);
+          DescribeNtSecurityAttributes(lpSecurity), ok);
   return ok;
 }

commit b9eb656e41c869818ea7fafe8f0e8f7d45a13b70

Fix Windows stdin regression
The non-blocking standard input feature was broken by ec957491e.

libc/calls/winstdin1.c
@@ -135,8 +135,8 @@ dontasan dontubsan dontinstrument textwindows void WinMainStdin(void) {
     NTTRACE("<stdin> CreateThread failed");
     return;
   }
-  g_fds.stdin.thread = hStdin;
-  g_fds.stdin.handle = hThread;
+  g_fds.stdin.handle = hStdin;
+  g_fds.stdin.thread = hThread;
   g_fds.stdin.reader = hReader;
   g_fds.stdin.writer = hWriter;
   g_fds.stdin.inisem = hSemaphore;

libc/intrin/kprintf.greg.c
@@ -330,7 +330,7 @@ privileged long kloghandle(void) {
                               kNtDuplicateSameAccess);
       } else if (n && n < 512) {
         hand = __imp_CreateFileW(
-            path, kNtGenericWrite | kNtFileAppendData,
+            path, kNtFileAppendData,
             kNtFileShareRead | kNtFileShareWrite | kNtFileShareDelete,
             &kNtIsInheritable, kNtOpenAlways, kNtFileAttributeNormal, 0);
       } else {

libc/runtime/winmain.greg.c
@@ -202,15 +202,15 @@ __msabi textwindows int64_t WinMain(int64_t hInstance, int64_t hPrevInstance,
   kStartTsc = rdtsc();
   __umask = 077;
   __pid = __imp_GetCurrentProcessId();
-  DeduplicateStdioHandles();
-  if (_weaken(WinMainStdin)) {
-    _weaken(WinMainStdin)();
-  }
   cmdline = MyCommandLine();
 #ifdef SYSDEBUG
   // sloppy flag-only check for early initialization
   if (__strstr16(cmdline, u"--strace")) ++__strace;
 #endif
+  DeduplicateStdioHandles();
+  if (_weaken(WinMainStdin)) {
+    _weaken(WinMainStdin)();
+  }
   if (_weaken(WinSockInit)) {
     _weaken(WinSockInit)();
   }

test/libc/calls/fcntl_test.c
@@ -214,3 +214,15 @@ TEST(posixAdvisoryLocks, twoProcesses) {
   ASSERT_SYS(0, 0, sigaction(SIGUSR1, &oldsa, 0));
   ASSERT_SYS(0, 0, sigprocmask(SIG_SETMASK, &oldss, 0));
 }
+
+TEST(fcntl, nonblock) {
+  int pfds[2];
+  char buf[8];
+  ASSERT_SYS(0, 0, pipe(pfds));
+  ASSERT_SYS(0, 0, fcntl(pfds[0], F_SETFL, O_RDONLY));
+  ASSERT_SYS(0, 0, fcntl(pfds[0], F_SETFL, O_RDONLY | O_NONBLOCK));
+  ASSERT_SYS(EAGAIN, -1, read(pfds[0], buf, 8));
+  ASSERT_SYS(0, 0, fcntl(pfds[0], F_SETFL, O_RDONLY));
+  ASSERT_SYS(0, 0, close(pfds[1]));
+  ASSERT_SYS(0, 0, close(pfds[0]));
+}