jart/cosmopolitan

commit 3b086af91b62833ba3715c8f50e14f9a848f5858

Fix issues for latest GCC toolchain

libc/calls/fstatat-nt.c
@@ -21,31 +21,40 @@
 #include "libc/calls/struct/stat.h"
 #include "libc/calls/struct/stat.internal.h"
 #include "libc/calls/syscall_support-nt.internal.h"
+#include "libc/errno.h"
 #include "libc/nt/createfile.h"
 #include "libc/nt/enum/accessmask.h"
 #include "libc/nt/enum/creationdisposition.h"
 #include "libc/nt/enum/fileflagandattributes.h"
 #include "libc/nt/enum/filesharemode.h"
+#include "libc/nt/errors.h"
 #include "libc/nt/runtime.h"
 #include "libc/sysv/consts/at.h"
 
 textwindows int sys_fstatat_nt(int dirfd, const char *path, struct stat *st,
                                int flags) {
-  int rc;
+  int rc, e;
   int64_t fh;
+  uint32_t dwDesiredAccess;
   uint16_t path16[PATH_MAX];
   if (__mkntpathat(dirfd, path, 0, path16) == -1) return -1;
   BLOCK_SIGNALS;
+  e = errno;
+  dwDesiredAccess = kNtFileGenericRead;
+TryAgain:
   if ((fh = CreateFile(
-           path16, kNtFileGenericRead,
-           kNtFileShareRead | kNtFileShareWrite | kNtFileShareDelete, 0,
-           kNtOpenExisting,
+           path16, dwDesiredAccess, 0, 0, kNtOpenExisting,
            kNtFileAttributeNormal | kNtFileFlagBackupSemantics |
                ((flags & AT_SYMLINK_NOFOLLOW) ? kNtFileFlagOpenReparsePoint
                                               : 0),
            0)) != -1) {
     rc = st ? sys_fstat_nt(fh, st) : 0;
     CloseHandle(fh);
+  } else if (dwDesiredAccess == kNtFileGenericRead &&
+             GetLastError() == kNtErrorSharingViolation) {
+    dwDesiredAccess = kNtFileReadAttributes;
+    errno = e;
+    goto TryAgain;
   } else {
     rc = __winerr();
   }

libc/stdio/dirstream.c
@@ -185,6 +185,10 @@ static textwindows uint8_t GetNtDirentType(struct NtWin32FindData *w) {
 
 static textwindows dontinline struct dirent *readdir_nt(DIR *dir) {
 TryAgain:
+  while (!dir->isdone &&
+         (dir->windata.dwFileAttributes & kNtFileAttributeSystem)) {
+    dir->isdone = !FindNextFile(dir->hand, &dir->windata);
+  }
   if (dir->isdone) {
     return NULL;
   }

libc/calls/fixenotdir.c
@@ -24,20 +24,13 @@
 #include "libc/str/str.h"
 
 static textwindows bool SubpathExistsThatsNotDirectory(char16_t *path) {
-  int e;
   char16_t *p;
   uint32_t attrs;
-  e = errno;
   while ((p = strrchr16(path, '\\'))) {
     *p = u'\0';
-    if ((attrs = GetFileAttributes(path)) != -1u) {
-      if (attrs & kNtFileAttributeDirectory) {
-        return false;
-      } else {
-        return true;
-      }
-    } else {
-      errno = e;
+    if ((attrs = GetFileAttributes(path)) != -1u &&
+        !(attrs & kNtFileAttributeDirectory)) {
+      return true;
     }
   }
   return false;

libc/calls/isdirectory-nt.c
@@ -26,15 +26,12 @@
  * Returns true if file exists and is a directory on Windows NT.
  */
 bool isdirectory_nt(const char *path) {
-  int e;
   uint32_t x;
   char16_t path16[PATH_MAX];
-  e = errno;
   if (__mkntpath(path, path16) == -1) return -1;
   if ((x = GetFileAttributes(path16)) != -1u) {
     return !!(x & kNtFileAttributeDirectory);
   } else {
-    errno = e;
     return false;
   }
 }

libc/calls/isregularfile-nt.c
@@ -26,15 +26,12 @@
  * Returns true if file exists and is a regular file on Windows NT.
  */
 bool isregularfile_nt(const char *path) {
-  int e;
   uint32_t x;
   char16_t path16[PATH_MAX];
-  e = errno;
   if (__mkntpath(path, path16) == -1) return -1;
   if ((x = GetFileAttributes(path16)) != -1u) {
     return !(x & (kNtFileAttributeDirectory | kNtFileAttributeReparsePoint));
   } else {
-    errno = e;
     return false;
   }
 }

libc/calls/issymlink-nt.c
@@ -16,9 +16,7 @@
 │ TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR             │
 │ PERFORMANCE OF THIS SOFTWARE.                                                │
 ╚─────────────────────────────────────────────────────────────────────────────*/
-#include "libc/calls/calls.h"
 #include "libc/calls/syscall_support-nt.internal.h"
-#include "libc/errno.h"
 #include "libc/nt/enum/fileflagandattributes.h"
 #include "libc/nt/files.h"
 
@@ -26,15 +24,12 @@
  * Returns true if file exists and is a symbolic link on Windows NT.
  */
 bool issymlink_nt(const char *path) {
-  int e;
   uint32_t x;
   char16_t path16[PATH_MAX];
-  e = errno;
   if (__mkntpath(path, path16) == -1) return -1;
   if ((x = GetFileAttributes(path16)) != -1u) {
     return !!(x & kNtFileAttributeReparsePoint);
   } else {
-    errno = e;
     return false;
   }
 }

libc/calls/mkntpathat.c
@@ -27,8 +27,6 @@
 #include "libc/sysv/consts/at.h"
 #include "libc/sysv/errfuns.h"
 
-__msabi extern typeof(GetFileAttributes) *const __imp_GetFileAttributesW;
-
 static int __mkntpathat_impl(int dirfd, const char *path, int flags,
                              char16_t file[hasatleast PATH_MAX]) {
   char16_t dir[PATH_MAX];
@@ -71,7 +69,7 @@ int __mkntpathat(int dirfd, const char *path, int flags,
     if (len > 1 && !(len == 3 && file[1] == ':')) {
       file[--len] = 0;
     }
-    if ((fattr = __imp_GetFileAttributesW(file)) != -1u &&
+    if ((fattr = GetFileAttributes(file)) != -1u &&
         !(fattr & kNtFileAttributeReparsePoint) &&
         !(fattr & kNtFileAttributeDirectory)) {
       return enotdir();

libc/calls/open-nt.c
@@ -57,7 +57,7 @@ static textwindows int64_t sys_open_nt_impl(int dirfd, const char *path,
   // you can't open symlinks; use readlink
   // this flag only applies to the final path component
   // if O_NOFOLLOW_ANY is passed (-1 on NT) it'll be rejected later
-  uint32_t fattr = __imp_GetFileAttributesW(path16);
+  uint32_t fattr = GetFileAttributes(path16);
   if (flags & O_NOFOLLOW) {
     if (fattr != -1u && (fattr & kNtFileAttributeReparsePoint)) {
       return eloop();

commit 3851025b77fcb8aa94df8bf8e42df3d71afa16ce

Fix SQLite regressions caused by 3b086af91

libc/calls/fcntl-nt.c
@@ -96,7 +96,7 @@ static textwindows bool OverlapsFileLock(struct FileLock *fl, int64_t off,
   EndA = off + (len - 1);
   BegB = fl->off;
   EndB = fl->off + (fl->len - 1);
-  return MAX(BegA, BegB) < MIN(EndA, EndB);
+  return MAX(BegA, BegB) <= MIN(EndA, EndB);
 }
 
 static textwindows bool EncompassesFileLock(struct FileLock *fl, int64_t off,

libc/calls/fstatat-nt.c
@@ -43,7 +43,9 @@ textwindows int sys_fstatat_nt(int dirfd, const char *path, struct stat *st,
   dwDesiredAccess = kNtFileGenericRead;
 TryAgain:
   if ((fh = CreateFile(
-           path16, dwDesiredAccess, 0, 0, kNtOpenExisting,
+           path16, dwDesiredAccess,
+           kNtFileShareRead | kNtFileShareWrite | kNtFileShareDelete, 0,
+           kNtOpenExisting,
            kNtFileAttributeNormal | kNtFileFlagBackupSemantics |
                ((flags & AT_SYMLINK_NOFOLLOW) ? kNtFileFlagOpenReparsePoint
                                               : 0),

libc/calls/pipe-nt.c
@@ -25,6 +25,7 @@
 #include "libc/nt/enum/accessmask.h"
 #include "libc/nt/enum/creationdisposition.h"
 #include "libc/nt/enum/fileflagandattributes.h"
+#include "libc/nt/enum/filesharemode.h"
 #include "libc/nt/ipc.h"
 #include "libc/nt/runtime.h"
 #include "libc/sysv/consts/limits.h"
@@ -57,8 +58,11 @@ static textwindows int sys_pipe_nt_impl(int pipefd[2], unsigned flags) {
                         mode, 1, PIPE_BUF, PIPE_BUF, 0, &kNtIsInheritable);
   __fds_lock();
   if (hin != -1) {
-    if ((hout = CreateFile(pipename, kNtGenericWrite, 0, &kNtIsInheritable,
-                           kNtOpenExisting, kNtFileFlagOverlapped, 0)) != -1) {
+    if ((hout = CreateFile(
+             pipename, kNtGenericWrite,
+             kNtFileShareRead | kNtFileShareWrite | kNtFileShareDelete,
+             &kNtIsInheritable, kNtOpenExisting, kNtFileFlagOverlapped, 0)) !=
+        -1) {
       g_fds.p[reader].kind = kFdFile;
       g_fds.p[reader].flags = O_RDONLY | flags;
       g_fds.p[reader].mode = 0010444;

libc/calls/truncate-nt.c
@@ -32,8 +32,10 @@ textwindows int sys_truncate_nt(const char *path, uint64_t length) {
   uint16_t path16[PATH_MAX];
   if (__mkntpath(path, path16) == -1) return -1;
   BLOCK_SIGNALS;
-  if ((fh = CreateFile(path16, kNtGenericWrite, kNtFileShareRead, NULL,
-                       kNtOpenExisting, kNtFileAttributeNormal, 0)) != -1) {
+  if ((fh = CreateFile(
+           path16, kNtGenericWrite,
+           kNtFileShareRead | kNtFileShareWrite | kNtFileShareDelete, NULL,
+           kNtOpenExisting, kNtFileAttributeNormal, 0)) != -1) {
     rc = sys_ftruncate_nt(fh, length);
     CloseHandle(fh);
   } else {

libc/intrin/kprintf.greg.c
@@ -1061,7 +1061,6 @@ privileged void kvprintf(const char *fmt, va_list v) {
 #pragma GCC diagnostic ignored "-Walloca-larger-than="
   long size = __get_safe_size(8000, 3000);
   if (size < 80) {
-    asm("int3");
     klog(STACK_ERROR, sizeof(STACK_ERROR) - 1);
     return;
   }

libc/sock/socketpair-nt.c
@@ -23,6 +23,7 @@
 #include "libc/nt/enum/accessmask.h"
 #include "libc/nt/enum/creationdisposition.h"
 #include "libc/nt/enum/fileflagandattributes.h"
+#include "libc/nt/enum/filesharemode.h"
 #include "libc/nt/ipc.h"
 #include "libc/nt/runtime.h"
 #include "libc/sock/internal.h"
@@ -73,7 +74,8 @@ textwindows int sys_socketpair_nt(int family, int type, int proto, int sv[2]) {
     return -1;
   }
 
-  h1 = CreateFile(pipename, kNtGenericWrite | kNtGenericRead, 0,
+  h1 = CreateFile(pipename, kNtGenericWrite | kNtGenericRead,
+                  kNtFileShareRead | kNtFileShareWrite | kNtFileShareDelete,
                   &kNtIsInheritable, kNtOpenExisting, kNtFileFlagOverlapped, 0);
 
   __fds_lock();

libc/stdio/dirstream.c
@@ -36,6 +36,7 @@
 #include "libc/nt/enum/accessmask.h"
 #include "libc/nt/enum/creationdisposition.h"
 #include "libc/nt/enum/fileflagandattributes.h"
+#include "libc/nt/enum/filesharemode.h"
 #include "libc/nt/enum/filetype.h"
 #include "libc/nt/files.h"
 #include "libc/nt/runtime.h"
@@ -229,10 +230,13 @@ TryAgain:
 
   // get inode such that it's consistent with stat()
   // it's important that we not follow symlinks here
-  int64_t fh = CreateFile(jp, kNtFileReadAttributes, 0, 0, kNtOpenExisting,
-                          kNtFileAttributeNormal | kNtFileFlagBackupSemantics |
-                              kNtFileFlagOpenReparsePoint,
-                          0);
+  int64_t fh =
+      CreateFile(jp, kNtFileReadAttributes,
+                 kNtFileShareRead | kNtFileShareWrite | kNtFileShareDelete, 0,
+                 kNtOpenExisting,
+                 kNtFileAttributeNormal | kNtFileFlagBackupSemantics |
+                     kNtFileFlagOpenReparsePoint,
+                 0);
   if (fh != kNtInvalidHandleValue) {
     struct NtByHandleFileInformation wst;
     if (GetFileInformationByHandle(fh, &wst)) {