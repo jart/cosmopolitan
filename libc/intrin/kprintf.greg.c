jart/cosmopolitan

commit ed17d3008bcd2c961618149f13fcd2d404327984

[metal] Add a uprintf() routine, for non-emergency boot logging (#905)
* [metal] Add a uprintf() routine, for non-emergency boot logging
* [metal] _Really_ push forward timing of VGA TTY initialization
* [metal] Do something useful with uprintf()
* [metal] Locate some ACPI tables, for later hardware detection

Specifically the code now tries to find the ACPI RSDP,
RSDT/XSDT, FADT, & MADT tables, whether in legacy BIOS
bootup mode or in a UEFI bootup.  These are useful for
figuring out how to (re)enable asynchronous interrupts
in legacy 8259 PIC mode.

libc/intrin/kprintf.greg.c
@@ -385,14 +385,29 @@ privileged long kloghandle(void) {
   return __klog_handle;
 }
 
+privileged void _klog_serial(const char *b, size_t n) {
+  size_t i;
+  uint16_t dx;
+  unsigned char al;
+  for (i = 0; i < n; ++i) {
+    for (;;) {
+      dx = 0x3F8 + UART_LSR;
+      asm("inb\t%1,%0" : "=a"(al) : "dN"(dx));
+      if (al & UART_TTYTXR) break;
+      asm("pause");
+    }
+    dx = 0x3F8;
+    asm volatile("outb\t%0,%1"
+                 : /* no inputs */
+                 : "a"(b[i]), "dN"(dx));
+  }
+}
+
 privileged void klog(const char *b, size_t n) {
 #ifdef __x86_64__
   int e;
   long h;
-  size_t i;
-  uint16_t dx;
   uint32_t wrote;
-  unsigned char al;
   long rax, rdi, rsi, rdx;
   if ((h = kloghandle()) == -1) {
     return;
@@ -407,18 +422,7 @@ privileged void klog(const char *b, size_t n) {
     if (_weaken(_klog_vga)) {
       _weaken(_klog_vga)(b, n);
     }
-    for (i = 0; i < n; ++i) {
-      for (;;) {
-        dx = 0x3F8 + UART_LSR;
-        asm("inb\t%1,%0" : "=a"(al) : "dN"(dx));
-        if (al & UART_TTYTXR) break;
-        asm("pause");
-      }
-      dx = 0x3F8;
-      asm volatile("outb\t%0,%1"
-                   : /* no inputs */
-                   : "a"(b[i]), "dN"(dx));
-    }
+    _klog_serial(b, n);
   } else {
     asm volatile("syscall"
                  : "=a"(rax), "=D"(rdi), "=S"(rsi), "=d"(rdx)
@@ -1152,3 +1156,6 @@ privileged void kprintf(const char *fmt, ...) {
   kvprintf(fmt, v);
   va_end(v);
 }
+
+__weak_reference(kprintf, uprintf);
+__weak_reference(kvprintf, uvprintf);

Makefile
@@ -170,11 +170,13 @@ include third_party/puff/puff.mk		# │
 include libc/elf/elf.mk				# │
 include ape/ape.mk				# │
 include libc/fmt/fmt.mk				# │
-include libc/vga/vga.mk				#─┘
+include libc/vga/vga.mk				# │
+include libc/irq/irq.mk				#─┘
 include libc/calls/calls.mk			#─┐
-include third_party/nsync/nsync.mk		# │
-include libc/runtime/runtime.mk			# ├──SYSTEMS RUNTIME
-include third_party/double-conversion/dc.mk	# │  You can issue system calls
+include libc/irq/irq.mk				# ├──SYSTEMS RUNTIME
+include third_party/nsync/nsync.mk		# │  You can issue system calls
+include libc/runtime/runtime.mk			# │
+include third_party/double-conversion/dc.mk	# │
 include libc/crt/crt.mk				# │
 include third_party/dlmalloc/dlmalloc.mk	#─┘
 include libc/mem/mem.mk				#─┐
@@ -374,6 +376,7 @@ COSMOPOLITAN_OBJECTS =			\
 	LIBC_RUNTIME			\
 	THIRD_PARTY_NSYNC		\
 	LIBC_ELF			\
+	LIBC_IRQ			\
 	LIBC_CALLS			\
 	LIBC_SYSV_CALLS			\
 	LIBC_VGA			\

examples/examples.mk
@@ -46,6 +46,7 @@ EXAMPLES_DIRECTDEPS =								\
 	LIBC_DNS								\
 	LIBC_FMT								\
 	LIBC_INTRIN								\
+	LIBC_IRQ								\
 	LIBC_LOG								\
 	LIBC_MEM								\
 	LIBC_NEXGEN32E								\

examples/vga2.c
@@ -26,6 +26,8 @@
 
 __static_yoink("vga_console");
 __static_yoink("_idt");
+__static_yoink("_AcpiMadtFlags");
+__static_yoink("_AcpiBootFlags");
 __static_yoink("EfiMain");
 
 int main(int argc, char *argv[]) {

libc/calls/metalfile.c
@@ -30,6 +30,7 @@
 #include "libc/calls/internal.h"
 #include "libc/calls/metalfile.internal.h"
 #include "libc/intrin/directmap.internal.h"
+#include "libc/intrin/kprintf.h"
 #include "libc/intrin/weaken.h"
 #include "libc/macros.internal.h"
 #include "libc/mem/mem.h"
@@ -74,6 +75,7 @@ textstartup void InitializeMetalFile(void) {
     memcpy(copied_base, (void *)(BANE + IMAGE_BASE_PHYSICAL), size);
     __ape_com_base = copied_base;
     __ape_com_size = size;
+    KINFOF("%s @ %p,+%#zx", APE_COM_NAME, copied_base, size);
   }
 }
 

libc/calls/metalfile_init.S
@@ -27,12 +27,12 @@
 #include "libc/macros.internal.h"
 #include "libc/calls/metalfile.internal.h"
 
-	.init.start 101,_init_metalfile
+	.init.start 102,_init_metalfile
 	push	%rdi
 	push	%rsi
 	call	InitializeMetalFile
 	pop	%rsi
 	pop	%rdi
-	.init.end 101,_init_metalfile
+	.init.end 102,_init_metalfile
 APE_COM_NAME:
 	.endobj	APE_COM_NAME,globl,hidden

libc/integral/c.inc
@@ -728,6 +728,8 @@ void abort(void) wontreturn;
 #endif
 
 #define __weak_reference(sym, alias)       \
+  __weak_reference_impl(sym, alias)
+#define __weak_reference_impl(sym, alias)  \
   __asm__(".weak\t" #alias "\n\t"          \
           ".equ\t" #alias ", " #sym "\n\t" \
           ".type\t" #alias ",@notype")

libc/intrin/interrupts.S
@@ -25,7 +25,9 @@
 │ OTHER DEALINGS IN THE SOFTWARE.                                              │
 ╚─────────────────────────────────────────────────────────────────────────────*/
 #include "libc/dce.h"
+#include "libc/intrin/kprintf.h"
 #include "libc/macros.internal.h"
+#include "libc/intrin/kprintf.h"
 #include "libc/runtime/pc.internal.h"
 
 //	Code and data structures for bare metal interrupt handling.

commit f86ffa1a25b04a9606c524fbf1852011d4286123

Fix aarch64 build breakage caused by ed17d3008

bin/cosmocc
@@ -227,7 +227,8 @@ for x; do
     continue
   fi
   if [ x"$x" = x"-" ] ||           # is an argument
-     [ x"$x" = x"${x#-*}" ]; then  # !startswith(x, "-")
+     [ x"$x" = x"${x#-*}" ] ||     # !startswith(x, "-")
+     [ x"$x" = x"-Wl,--version" ]; then
     GOT_SOME=1
   elif [ x"$x" = x"-static-libstdc++" ]; then
     continue

libc/intrin/kprintf.greg.c
@@ -385,6 +385,7 @@ privileged long kloghandle(void) {
   return __klog_handle;
 }
 
+#ifdef __x86_64__
 privileged void _klog_serial(const char *b, size_t n) {
   size_t i;
   uint16_t dx;
@@ -402,6 +403,7 @@ privileged void _klog_serial(const char *b, size_t n) {
                  : "a"(b[i]), "dN"(dx));
   }
 }
+#endif /* __x86_64__ */
 
 privileged void klog(const char *b, size_t n) {
 #ifdef __x86_64__