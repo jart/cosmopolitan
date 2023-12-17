1889fd1c30541aa4cd3a03249b390c91aa818ba5
Jōshin <git@wholezero.org>
2023-12-16 23:00:04 -0500

    modeline: noet, BUILD.mk

--- third_party/less/BUILD.mk
+++ third_party/less/BUILD.mk
@@ -1,5 +1,5 @@
 #-*-mode:lessfile-gless;indent-tabs-mode:t;tab-width:8;coding:utf-8-*-┐
-#── vi: set et ft=less ts=8 sw=8 fenc=utf-8 :vi ──────────────────────┘
+#── vi: set noet ft=less ts=8 sw=8 fenc=utf-8 :vi ────────────────────┘
 
 PKGS += THIRD_PARTY_LESS
 
