357fcb9c64bf6b2c8a38844e2b174e060c310d01
Jōshin <git@wholezero.org>
2023-12-18 17:38:12 -0500

    Fix indentation, make llvm happy
    clang says "label at end of switch statement is a C2x extension."

--- libc/calls/getprogramexecutablename.greg.c
+++ libc/calls/getprogramexecutablename.greg.c
@@ -58,8 +58,8 @@ static inline int AllNumDot(const char *s) {
       case 0:   return 1;
       case '0': case '1': case '2': case '3': case '4':
       case '5': case '6': case '7': case '8': case '9': case '.':
-        /* continue */
-      }
+        ; /* continue */
+    }
   }
 }
 
