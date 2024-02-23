057e9e05a6b059c42a95926900d5ba4bcf6bb961
Ivan Komarov <Ivan.Komarov@dfyz.info>
2024-02-23 13:11:13 +0100

    Update the copyright for the new test file
    Suggested in https://github.com/jart/cosmopolitan/pull/1109#pullrequestreview-1897331016

--- test/libc/stdio/fscanf_test.c
+++ test/libc/stdio/fscanf_test.c
@@ -1,7 +1,7 @@
 /*-*- mode:c;indent-tabs-mode:nil;c-basic-offset:2;tab-width:8;coding:utf-8 -*-│
 │ vi: set et ft=c ts=2 sts=2 sw=2 fenc=utf-8                               :vi │
 ╞══════════════════════════════════════════════════════════════════════════════╡
-│ Copyright 2022 Justine Alexandra Roberts Tunney                              │
+│ Copyright 2024 Ivan Komarov                                                  │
 │                                                                              │
 │ Permission to use, copy, modify, and/or distribute this software for         │
 │ any purpose with or without fee is hereby granted, provided that the         │
