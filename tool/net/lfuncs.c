fab02d9e5fba542d254757aff8379ca74ef4efaa
BONNAURE Olivier <o.bonnaure@solisoft.net>
2024-04-12 15:42:30 +0200

    refactor code and less use of _rand64

--- tool/net/lfuncs.c
+++ tool/net/lfuncs.c
@@ -833,8 +833,8 @@ int LuaUuidV4(lua_State *L) {
   char v[] = {'0', '1', '2', '3', '4', '5', '6', '7',
               '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
   char uuid_str[37] = {0};
-  uint64_t r = 0;
-  for (int i = 0, j = 16; i < 36; ++i, ++j) {
+  uint64_t r = _rand();
+  for (int i = 0, j = 0; i < 36; ++i, ++j) {
 
     if (j == 16) {
       r = _rand64();
