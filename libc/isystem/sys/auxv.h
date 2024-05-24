5f61d273e4fb704d6fdc00de84c14c98776b4280
Justine Tunney <jtunney@gmail.com>
2024-05-24 11:44:31 -0700

    Add hwap constants to sys/auxv.h

--- libc/isystem/sys/auxv.h
+++ libc/isystem/sys/auxv.h
@@ -2,4 +2,5 @@
 #define COSMOPOLITAN_LIBC_ISYSTEM_SYS_AUXV_H_
 #include "libc/runtime/runtime.h"
 #include "libc/sysv/consts/auxv.h"
+#include "libc/sysv/consts/hwcap.h"
 #endif /* COSMOPOLITAN_LIBC_ISYSTEM_SYS_AUXV_H_ */
