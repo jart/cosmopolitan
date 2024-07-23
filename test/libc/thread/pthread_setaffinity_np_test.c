61c36c1dd6a5ac16077357daf67775661b6fbd82
Justine Tunney <jtunney@gmail.com>
2024-07-22 18:41:45 -0700

    Allow pthread_condattr_setpshared() to set shared

--- test/libc/thread/pthread_setaffinity_np_test.c
+++ test/libc/thread/pthread_setaffinity_np_test.c
@@ -1,7 +1,7 @@
 /*-*- mode:c;indent-tabs-mode:nil;c-basic-offset:2;tab-width:8;coding:utf-8 -*-│
 │ vi: set et ft=c ts=2 sts=2 sw=2 fenc=utf-8                               :vi │
 ╞══════════════════════════════════════════════════════════════════════════════╡
-│ Copyright 2022 Justine Alexandra Roberts Tunney                              │
+│ Copyright 2024 Justine Alexandra Roberts Tunney                              │
 │                                                                              │
 │ Permission to use, copy, modify, and/or distribute this software for         │
 │ any purpose with or without fee is hereby granted, provided that the         │
@@ -16,25 +16,44 @@
 │ TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR             │
 │ PERFORMANCE OF THIS SOFTWARE.                                                │
 ╚─────────────────────────────────────────────────────────────────────────────*/
-#include "libc/errno.h"
+#include "libc/calls/struct/cpuset.h"
+#include "libc/dce.h"
+#include "libc/runtime/runtime.h"
+#include "libc/testlib/testlib.h"
 #include "libc/thread/thread.h"
+#include "libc/thread/thread2.h"
 
-/**
- * Sets condition process sharing.
- *
- * @param pshared can be one of
- *     - `PTHREAD_PROCESS_PRIVATE` (default)
- *     - `PTHREAD_PROCESS_SHARED`
- * @return 0 on success, or error on failure
- * @raises EINVAL if `pshared` is invalid
- */
-errno_t pthread_condattr_setpshared(pthread_condattr_t *attr, int pshared) {
-  switch (pshared) {
-    case PTHREAD_PROCESS_SHARED:
-    case PTHREAD_PROCESS_PRIVATE:
-      *attr = pshared;
-      return 0;
-    default:
-      return EINVAL;
-  }
+TEST(pthread_setaffinity_np, test) {
+
+  // works on almost nothing
+  if (IsXnu())
+    return;
+  if (IsNetbsd())
+    return;
+  if (IsOpenbsd())
+    return;
+  if (__get_cpu_count() < 2)
+    return;
+
+  // save bitset
+  cpu_set_t old;
+  if (!IsWindows())
+    ASSERT_EQ(0, pthread_getaffinity_np(pthread_self(), sizeof(old), &old));
+
+  // go to first cpu
+  cpu_set_t bitset;
+  CPU_ZERO(&bitset);
+  CPU_SET(0, &bitset);
+  ASSERT_EQ(0, pthread_setaffinity_np(pthread_self(), sizeof(bitset), &bitset));
+  ASSERT_EQ(0, sched_getcpu());
+
+  // go to second cpu
+  CPU_ZERO(&bitset);
+  CPU_SET(1, &bitset);
+  ASSERT_EQ(0, pthread_setaffinity_np(pthread_self(), sizeof(bitset), &bitset));
+  ASSERT_EQ(1, sched_getcpu());
+
+  // restore bitset
+  if (!IsWindows())
+    ASSERT_EQ(0, pthread_setaffinity_np(pthread_self(), sizeof(old), &old));
 }
