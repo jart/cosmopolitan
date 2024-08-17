77be4602908c8a921113005747a3a7e17b3228e4
Justine Tunney <jtunney@gmail.com>
2024-08-17 06:32:10 -0700

    Make Windows REPLs great again

--- third_party/linenoise/linenoise.c
+++ third_party/linenoise/linenoise.c
@@ -408,9 +408,7 @@ static int linenoiseIsUnsupportedTerm(void) {
   char *term;
   static char once, res;
   if (!once) {
-    if (IsWindows()) {
-      res = 1;
-    } else if ((term = getenv("TERM"))) {
+    if ((term = getenv("TERM"))) {
       for (i = 0; i < sizeof(kUnsupported) / sizeof(*kUnsupported); i++) {
         if (!strcasecmp(term, kUnsupported[i])) {
           res = 1;
