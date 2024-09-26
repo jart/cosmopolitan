70603fa6ea9c01c9e770f6254af1fed58da21e7e
Justine Tunney <jtunney@gmail.com>
2024-09-26 04:42:41 -0700

    Fix makedev() prototype
    Fixes #1281

--- libc/calls/makedev.h
+++ libc/calls/makedev.h
@@ -1,5 +1,6 @@
 #ifndef COSMOPOLITAN_LIBC_CALLS_MAKEDEV_H_
 #define COSMOPOLITAN_LIBC_CALLS_MAKEDEV_H_
+COSMOPOLITAN_C_START_
 
 uint64_t makedev(uint32_t, uint32_t) libcesque;
 uint32_t major(uint64_t) libcesque;
@@ -9,4 +10,5 @@ uint32_t minor(uint64_t) libcesque;
 #define minor(x)      minor(x)
 #define makedev(x, y) makedev(x, y)
 
+COSMOPOLITAN_C_END_
 #endif /* COSMOPOLITAN_LIBC_CALLS_MAKEDEV_H_ */
