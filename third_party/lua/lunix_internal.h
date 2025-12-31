d2d673997e63b06b31249f95357f92ac2979cd1d
Will Maier <189851+whilp@users.noreply.github.com>
2025-12-31 22:39:11 +0000

    lua: add lunix_internal.h for shared helpers
    Preparation for splitting lunix.c into smaller files.

---
+++ third_party/lua/lunix_internal.h
@@ -0,0 +1,32 @@
+#ifndef COSMOPOLITAN_THIRD_PARTY_LUA_LUNIX_INTERNAL_H_
+#define COSMOPOLITAN_THIRD_PARTY_LUA_LUNIX_INTERNAL_H_
+#include "libc/calls/struct/rusage.h"
+#include "libc/calls/struct/sigset.h"
+#include "libc/calls/struct/stat.h"
+#include "libc/calls/struct/statfs.h"
+#include "third_party/lua/lauxlib.h"
+#include "third_party/lua/lua.h"
+#include "third_party/lua/lunix.h"
+COSMOPOLITAN_C_START_
+
+extern lua_State *GL;
+
+void LuaSetIntField(lua_State *, const char *, lua_Integer);
+void LuaPushSigset(lua_State *, sigset_t);
+void LuaPushStat(lua_State *, struct stat *);
+void LuaPushStatfs(lua_State *, struct statfs *);
+void LuaPushRusage(lua_State *, struct rusage *);
+
+int SysretBool(lua_State *, const char *, int, int);
+int SysretInteger(lua_State *, const char *, int, int64_t);
+
+void LuaUnixStatObj(lua_State *);
+void LuaUnixStatfsObj(lua_State *);
+void LuaUnixRusageObj(lua_State *);
+void LuaUnixErrnoObj(lua_State *);
+void LuaUnixMemoryObj(lua_State *);
+void LuaUnixSigsetObj(lua_State *);
+void LuaUnixDirObj(lua_State *);
+
+COSMOPOLITAN_C_END_
+#endif /* COSMOPOLITAN_THIRD_PARTY_LUA_LUNIX_INTERNAL_H_ */
