a57238ee46be3e32ecc5589576e7e51b7ee10a80
Gavin Hayes <gavin@computoid.com>
2026-08-31 01:48:29 -0400

    fix: only use end of __program_executable_name as stack top if it's greater than the other methods
    This fix isn't strickly needed now, but makes the code more resilient to changes in the ape loader.

--- libc/intrin/getmainstack.c
+++ libc/intrin/getmainstack.c
@@ -74,16 +74,19 @@ static int __get_length(const char *s) {
 }
 
 static uintptr_t __get_main_top(int pagesz) {
-  uintptr_t top;
+  uintptr_t top, alttop;
   const char *s;
-  if ((s = __get_last(__envp)) || (s = __get_last(__argv)) ||
-      (s = __program_executable_name)) {
+  if ((s = __get_last(__envp)) || (s = __get_last(__argv))) {
     top = (uintptr_t)s + __get_length(s);
   } else {
     unsigned long *xp = __auxv;
     while (*xp)
       xp += 2;
     top = (uintptr_t)xp;
+  }
+  if ((s = __program_executable_name)) {
+    alttop = (uintptr_t)s + __get_length(s);
+    top = MAX(top, alttop);
   }
   return ROUNDUP(top, pagesz);
 }
